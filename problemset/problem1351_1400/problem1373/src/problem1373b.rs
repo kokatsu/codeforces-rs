da26aef8df3c16d4dff0c42524c8037fbf054429
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-21 18:25:19 +0900

    solve problem1373b

---
+++ problemset/problem1351_1400/problem1373/src/problem1373b.rs
@@ -0,0 +1,43 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: i64 = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let s: String = read_string();
+
+        let l: usize = s.len();
+        let m: usize = s.chars()
+                        .filter(|&x| x == '0')
+                        .collect::<Vec<_>>()
+                        .len();
+        let n: usize = m.min(l-m);
+
+        let res: &str = if n % 2 == 1 {"DA"} else {"NET"};
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
