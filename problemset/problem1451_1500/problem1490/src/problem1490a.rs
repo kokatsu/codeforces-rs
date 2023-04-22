321fa7b04a6fc3d3048b8a6ec44583bc1627a868
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-04-22 12:00:02 +0900

    solve problem1490a

---
+++ problemset/problem1451_1500/problem1490/src/problem1490a.rs
@@ -0,0 +1,45 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<i64> = read_vec();
+
+        let mut res: i64 = 0;
+        for i in 1..n {
+            let mut x: i64 = a[i].min(a[i-1]);
+            let y: i64 = a[i].max(a[i-1]);
+            while x * 2 < y {
+                res += 1;
+                x *= 2;
+            }
+        }
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
