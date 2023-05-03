e55a40765c68a270a8f73468bced0062a6c66a32
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-03 21:15:45 +0900

    solve problem1675a

---
+++ problemset/problem1651_1700/problem1675/src/problem1675a.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let input: Vec<i64> = read_vec();
+        let a: i64 = input[0];
+        let b: i64 = input[1];
+        let c: i64 = input[2];
+        let x: i64 = input[3];
+        let y: i64 = input[4];
+
+        let res: &str =
+            if (x-a).max(0) + (y-b).max(0) <= c {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
