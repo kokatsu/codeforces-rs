df5c7d6a3e3ec90a1b8fe3da14385637edfaed1b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-07 20:23:42 +0900

    solve problem0822a

---
+++ problemset/problem0801_0850/problem0822/src/problem0822a.rs
@@ -0,0 +1,37 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let input: Vec<i64> = read_vec();
+    let a: i64 = input[0];
+    let b: i64 = input[1];
+
+    let m: i64 = a.min(b);
+    let mut res: i64 = 1;
+    for i in 1..=m {
+        res *= i;
+    }
+
+    let mut out = BufWriter::new(stdout().lock());
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
