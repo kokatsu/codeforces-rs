073a29d6b3a791902f231a5a93a9dc27747a5dff
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-15 18:48:38 +0900

    solve problem1622a

---
+++ problemset/problem1601_1650/problem1622/src/problem1622a.rs
@@ -0,0 +1,50 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let mut l: Vec<i64> = read_vec();
+
+        l.sort();
+
+        let res: &str =
+            if l[0] + l[1] == l[2] {
+                "YES"
+            }
+            else if l[0] == l[1] && l[2] % 2 == 0 {
+                "YES"
+            }
+            else if l[0] % 2 == 0 && l[1] == l[2] {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
