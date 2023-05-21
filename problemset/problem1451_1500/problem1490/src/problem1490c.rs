d794b4dbee33c52848d3889be19056a40d4da500
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-21 19:46:43 +0900

    solve problem1490c

---
+++ problemset/problem1451_1500/problem1490/src/problem1490c.rs
@@ -0,0 +1,58 @@
+use std::io::{stdout, Write, BufWriter};
+use std::collections::HashSet;
+
+const N: i64 = 10000i64;
+
+fn main() {
+    let t: usize = read();
+
+    let mut set: HashSet<i64> = HashSet::new();
+    for i in 1..=N {
+        set.insert(i*i*i);
+    }
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let x: i64 = read();
+
+        let mut is_ok: bool = false;
+        for i in 1..=N {
+            let a: i64 = i * i * i;
+
+            if a >= x {
+                break;
+            }
+
+            if set.contains(&(x-a)) {
+                is_ok = true;
+                break;
+            }
+        }
+
+        let res: &str = if is_ok {"YES"} else {"NO"};
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
