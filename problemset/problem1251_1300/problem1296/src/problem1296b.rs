56d485d4526e0c2e5e9a0ef27f2b4ae0e1fc1942
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-23 22:07:03 +0900

    solve problem1296b

---
+++ problemset/problem1251_1300/problem1296/src/problem1296b.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, Write, BufWriter};
+
+const D: i64 = 10;
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let mut s: i64 = read();
+
+        let mut res: i64 = 0;
+        while s >= D {
+            let d = s / D;
+            let r = s % D;
+
+            res += d * D;
+            s = d + r;
+        }
+
+        res += s;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
