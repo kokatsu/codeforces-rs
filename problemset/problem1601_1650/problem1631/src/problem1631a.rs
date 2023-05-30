df0e9cec9b205d5525c52c820471d22c8aad101b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-05-30 21:48:31 +0900

    solve problem1631a

---
+++ problemset/problem1601_1650/problem1631/src/problem1631a.rs
@@ -0,0 +1,49 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let a: Vec<i64> = read_vec();
+        let b: Vec<i64> = read_vec();
+
+        let (x, y): (i64, i64) = a.iter()
+                                    .zip(b.iter())
+                                    .fold((-1, -1), |(x, y), (&u, &v)| {
+                                        if u < v {
+                                            (x.max(u), y.max(v))
+                                        }
+                                        else {
+                                            (x.max(v), y.max(u))
+                                        }
+                                    });
+
+        let res: i64 = x * y;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
