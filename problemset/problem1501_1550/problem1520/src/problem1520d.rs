6c7c1ac161389a9f2fc73204ba8f71863090a529
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-06-05 20:16:34 +0900

    solve problem1520d

---
+++ problemset/problem1501_1550/problem1520/src/problem1520d.rs
@@ -0,0 +1,43 @@
+use std::io::{stdout, Write, BufWriter};
+use std::collections::HashMap;
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<i64> = read_vec();
+
+        let map: HashMap<i64, i64> = (0..n).fold(HashMap::new(), |mut map, i| {
+                                                *map.entry(a[i]-(i as i64)).or_insert(0) += 1;
+                                                map
+                                            });
+
+        let res: i64 = map.values().fold(0, |res, val| res + val * (val - 1) / 2);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
