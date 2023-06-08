0bed0adf6afecc33cbb2e34ee9b078dbd991b50f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-06-08 21:13:10 +0900

    solve problem1521a

---
+++ problemset/problem1501_1550/problem1521/src/problem1521a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let input: Vec<i64> = read_vec();
+        let a: i64 = input[0];
+        let b: i64 = input[1];
+
+        if b > 1 {
+            writeln!(out, "YES").unwrap();
+            writeln!(out, "{} {} {}", a, a*b, a*(b+1)).unwrap();
+        }
+        else {
+            writeln!(out, "NO").unwrap();
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
