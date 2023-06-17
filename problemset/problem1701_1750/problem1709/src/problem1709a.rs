8850bef06f9340df698130f2ad21448fb9c9af3f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-06-17 20:57:07 +0900

    solve problem1709a

---
+++ problemset/problem1701_1750/problem1709/src/problem1709a.rs
@@ -0,0 +1,49 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let mut x: usize = read();
+        let a: Vec<usize> = read_vec();
+
+        let mut count: usize = 0;
+        while x > 0 && x != a[x-1] {
+            x = a[x-1];
+            count += 1;
+        }
+
+        let res: &str =
+            if count == 3 {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
