12e7a5685485adfbba74c2e2a2d9cf21690e29f4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-06-19 19:32:51 +0900

    solve problem1535a

---
+++ problemset/problem1501_1550/problem1535/src/problem1535a.rs
@@ -0,0 +1,48 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let mut a: Vec<i64> = read_vec();
+
+        for i in 0..2 {
+            if a[i*2] < a[i*2+1] {
+                a.swap(i*2, i*2+1);
+            }
+        }
+
+        let res: &str =
+            if a[0] > a[3] && a[2] > a[1] {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
