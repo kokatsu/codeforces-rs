fb069cf6a17d858ad7315cbec3086e5086f85d79
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-06-23 19:41:07 +0900

    solve problem1341a

---
+++ problemset/problem1301_1350/problem1341/src/problem1341a.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let input: Vec<i64> = read_vec();
+        let n: i64 = input[0];
+        let a: i64 = input[1];
+        let b: i64 = input[2];
+        let c: i64 = input[3];
+        let d: i64 = input[4];
+
+        let res: &str =
+            if (a - b) * n <= c + d && c - d <= (a + b) * n {
+                "Yes"
+            }
+            else {
+                "No"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
