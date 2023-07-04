ffaf5d0e3b7073e135831803d71551a2d3135586
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-07-04 21:57:47 +0900

    solve problem0977a

---
+++ problemset/problem0951_1000/problem0977/src/problem0977a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let input: Vec<i64> = read_vec();
+    let n: i64 = input[0];
+    let k: i64 = input[1];
+
+    let res: i64 =
+        (0..k)
+        .fold(n, |res, _| {
+            if res % 10 == 0 {
+                res / 10
+            }
+            else {
+                res - 1
+            }
+        });
+
+    let mut out = BufWriter::new(stdout().lock());
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
