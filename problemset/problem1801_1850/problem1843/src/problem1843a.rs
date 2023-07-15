3799d2adf88a44c3786c6af4ad094bb58b852aea
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-07-15 20:51:47 +0900

    solve problem1843a

---
+++ problemset/problem1801_1850/problem1843/src/problem1843a.rs
@@ -0,0 +1,39 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let n: usize = read();
+        let mut a: Vec<usize> = read_vec();
+
+        a.sort();
+
+        let res: usize = a[n-n/2..n].iter().sum::<usize>() - a[0..n/2].iter().sum::<usize>();
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
