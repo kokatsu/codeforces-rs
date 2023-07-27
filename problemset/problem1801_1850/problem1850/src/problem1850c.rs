6fb531a4625f81762a9db33f43d3dc4185446f81
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-07-27 12:30:41 +0900

    solve problem1850c

---
+++ problemset/problem1801_1850/problem1850/src/problem1850c.rs
@@ -0,0 +1,39 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let res: String =
+            (0..8)
+            .fold(String::new(), |res, _| {
+                let s: String = read_string();
+                res + &s.chars().filter(|&x| x != '.').into_iter().collect::<String>()
+            });
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
