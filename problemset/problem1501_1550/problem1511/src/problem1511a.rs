930058496dcaa7f5580da604762ec1478a564fb5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-07-30 22:13:00 +0900

    solve problem1511a

---
+++ problemset/problem1501_1550/problem1511/src/problem1511a.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let r: Vec<usize> = read_vec();
+
+        let res: usize =
+            r
+            .iter()
+            .fold(0, |res, x| {
+                if x % 2 == 1 {
+                    res + 1
+                }
+                else {
+                    res
+                }
+            });
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
