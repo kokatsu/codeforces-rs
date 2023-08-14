b4ceac7aba3c4f4a34b9128e9199271ef5997ba7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-15 00:42:10 +0900

    solve problem0349a

---
+++ problemset/problem0301_0350/problem0349/src/problem0349a.rs
@@ -0,0 +1,66 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let n: usize = read();
+    let a: Vec<usize> = read_vec();
+
+    let num: usize =
+        a
+        .iter()
+        .fold((0, 0, 0), |(num, u, v), x| {
+            if *x == 25 {
+                (num+1, u+1, v)
+            }
+            else if *x == 50 {
+                if u > 0 {
+                    (num+1, u-1, v+1)
+                }
+                else {
+                    (num, u, v)
+                }
+            }
+            else {
+                if u > 0 && v > 0 {
+                    (num+1, u-1, v-1)
+                }
+                else if u > 2 {
+                    (num+1, u-3, v)
+                }
+                else {
+                    (num, u, v)
+                }
+            }
+        }).0;
+
+    let res: &str =
+        if num == n {
+            "YES"
+        }
+        else {
+            "NO"
+        };
+
+    let mut out = BufWriter::new(stdout().lock());
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
