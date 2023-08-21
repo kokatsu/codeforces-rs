af36212e0da1b9351ad43213e5a2988aa4c01e0e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-21 23:35:49 +0900

    solve problem1644a

---
+++ problemset/problem1601_1650/problem1644/src/problem1644a.rs
@@ -0,0 +1,50 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let rgb: Vec<char> = vec!{'r', 'g', 'b'};
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let s: String = read_string();
+
+        let res: &str =
+            rgb
+            .iter()
+            .fold("YES", |res, x| {
+                let l: usize = s.find(*x).unwrap();
+                let u: usize = s.find(x.to_ascii_uppercase()).unwrap();
+                if l < u {
+                    res
+                }
+                else {
+                    "NO"
+                }
+            });
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
