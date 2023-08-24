1f2f4acbfed80827b3e860a0ed7f92c1bb2b1c4c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-25 00:34:41 +0900

    solve problem1807c

---
+++ problemset/problem1801_1850/problem1807/src/problem1807c.rs
@@ -0,0 +1,64 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let s: String = read_string();
+
+        let (even, odd): (Vec<i64>, Vec<i64>) =
+            s
+            .chars()
+            .enumerate()
+            .fold((vec![0; 26], vec![0; 26]), |(mut even, mut odd), (i, x)| {
+                if i % 2 == 0 {
+                    even[((x as u8)-('a' as u8)) as usize] += 1;
+                }
+                else {
+                    odd[((x as u8)-('a' as u8)) as usize] += 1;
+                }
+                (even, odd)
+            });
+
+        let num: usize =
+            even
+            .iter()
+            .zip(odd.iter())
+            .filter(|(x, y)| x > &&0 && y > &&0)
+            .count();
+
+        let res: &str =
+            if num == 0 {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
