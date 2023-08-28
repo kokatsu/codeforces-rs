16d8fce8b0971380134882efc644267955c193e2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-08-29 00:30:34 +0900

    solve problem1324b

---
+++ problemset/problem1301_1350/problem1324/src/problem1324b.rs
@@ -0,0 +1,50 @@
+use std::io::{stdout, Write, BufWriter};
+use std::collections::HashSet;
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<usize> = read_vec();
+
+        let mut set: HashSet<usize> = HashSet::new();
+
+        let res: &str =
+            (1..n)
+            .fold("NO", |res, i| {
+                if set.contains(&a[i]) {
+                    "YES"
+                }
+                else {
+                    set.insert(a[i-1]);
+                    res
+                }
+            });
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
