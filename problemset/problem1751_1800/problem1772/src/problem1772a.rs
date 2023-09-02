f4cc3fbb668f7dcd7ca97f2b8c4017b4164da66e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-02 23:55:49 +0900

    solve problem1772a

---
+++ problemset/problem1751_1800/problem1772/src/problem1772a.rs
@@ -0,0 +1,36 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let s: Vec<char> = read_string().chars().collect();
+
+        let res: u8 = (s[0] as u8) + (s[2] as u8) - ('0' as u8) * 2;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
