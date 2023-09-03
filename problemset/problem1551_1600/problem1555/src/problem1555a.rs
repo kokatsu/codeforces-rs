5e0bdb585b6ff472b16eacff493be64d4937c9a0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-03 23:51:37 +0900

    solve problem1555a

---
+++ problemset/problem1551_1600/problem1555/src/problem1555a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let n: u64 = read();
+
+        let res: u64 =
+            if n >= 6 {
+                (n + 1) / 2 * 5
+            }
+            else {
+                15
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
