f03b86ab97d1a82831787f2883475271f96fcf18
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-09-05 00:13:11 +0900

    solve problem1382a

---
+++ problemset/problem1351_1400/problem1382/src/problem1382a.rs
@@ -0,0 +1,48 @@
+use std::io::{stdout, Write, BufWriter};
+use std::collections::HashSet;
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let _input: Vec<usize> = read_vec();
+        let a: Vec<u32> = read_vec();
+        let b: HashSet<u32> = read_vec::<u32>().iter().cloned().collect();
+
+        let mut exist: bool = false;
+        for x in a.iter() {
+            if b.contains(&x) && !exist {
+                writeln!(out, "YES").unwrap();
+                writeln!(out, "1 {}", x).unwrap();
+                exist = true;
+            }
+        }
+
+        if !exist {
+            writeln!(out, "NO").unwrap();
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
