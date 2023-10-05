2ce93e9d4c5749bd7b4e5aac50743726d7289292
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-05 22:08:19 +0900

    solve problem1650b

---
+++ problemset/problem1601_1650/problem1650/src/problem1650b.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let input: Vec<u64> = read_vec();
+        let l: u64 = input[0];
+        let r: u64 = input[1];
+        let a: u64 = input[2];
+
+        let m: u64 = r / a * a - 1;
+
+        let res: u64 =
+            if l <= m && m <= r {
+                (r/a+r%a).max(m/a+m%a)
+            }
+            else {
+                r / a + r % a
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
