623cf94b58eb921f00b488c34eb528a19cb5ecb0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-08 22:51:04 +0900

    solve problem1469a

---
+++ problemset/problem1451_1500/problem1469/src/problem1469a.rs
@@ -0,0 +1,44 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let s: Vec<char> = read_string().chars().collect();
+
+        let l: usize = s.len();
+
+        let res: &str =
+            if s[0] != ')' && s[l-1] != '(' && l % 2 == 0 {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
