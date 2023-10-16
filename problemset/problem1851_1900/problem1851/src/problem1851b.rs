5f45a95dd11fd183ce2f2892398614ab10ccebe6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-17 01:29:14 +0900

    solve problem1851b

---
+++ problemset/problem1851_1900/problem1851/src/problem1851b.rs
@@ -0,0 +1,51 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<u64> = read_vec();
+
+        let mut b: Vec<u64> = a.to_vec();
+        b.sort();
+
+        let c: usize =
+            (0..n)
+            .filter(|&i| a[i] != b[i] && a[i] % 2 != b[i] % 2)
+            .count();
+
+        let res: &str =
+            if c == 0 {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
