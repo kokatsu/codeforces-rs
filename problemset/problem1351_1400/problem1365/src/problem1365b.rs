b5af397259219a98069e1f7b428bc075be7f1e92
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-24 00:37:12 +0900

    solve problem1365b

---
+++ problemset/problem1351_1400/problem1365/src/problem1365b.rs
@@ -0,0 +1,53 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<usize> = read_vec();
+        let b: Vec<usize> = read_vec();
+
+        let is_sorted: bool =
+            (0..n-1)
+            .all(|i| a[i] <= a[i+1]);
+
+        let can_swap: bool =
+            b
+            .iter()
+            .any(|&x| x != b[0]);
+
+        let res: &str =
+            if is_sorted || can_swap {
+                "Yes"
+            }
+            else {
+                "No"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
