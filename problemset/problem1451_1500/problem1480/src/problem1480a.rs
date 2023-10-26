1be6af6618368c495217193b9e3011963f13a3f6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-10-27 02:20:54 +0900

    solve problem1480a

---
+++ problemset/problem1451_1500/problem1480/src/problem1480a.rs
@@ -0,0 +1,59 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let s: String = read_string();
+
+        let res: String =
+            s
+            .chars()
+            .enumerate()
+            .fold(String::new(), |res, (i, c)| {
+                let d: char =
+                    if i % 2 == 0 {
+                        if c == 'a' {
+                            'b'
+                        }
+                        else {
+                            'a'
+                        }
+                    }
+                    else {
+                        if c == 'z' {
+                            'y'
+                        }
+                        else {
+                            'z'
+                        }
+                    };
+                res + &d.to_string()
+            });
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
