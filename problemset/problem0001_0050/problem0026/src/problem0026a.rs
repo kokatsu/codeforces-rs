ab3779b6e5582d1ffdc8b6e5ed4d44c4bad20d8c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-04 23:47:57 +0900

    solve problem0026a

---
+++ problemset/problem0001_0050/problem0026/src/problem0026a.rs
@@ -0,0 +1,44 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let n: usize = read();
+
+    let mut counts: Vec<u64> = vec![0; n+1];
+    for i in 2..=n {
+        if counts[i] == 0 {
+            for j in (i*2..=n).step_by(i) {
+                counts[j] += 1;
+            }
+        }
+    }
+
+    let res: usize =
+        counts
+        .iter()
+        .filter(|&&x| x == 2)
+        .count();
+
+    let mut out = BufWriter::new(stdout().lock());
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
