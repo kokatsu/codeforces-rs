44d09f9450c670e837d683a37061f1c9b863940b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-13 01:09:50 +0900

    solve problem1543a

---
+++ problemset/problem1501_1550/problem1543/src/problem1543a.rs
@@ -0,0 +1,44 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let input: Vec<u64> = read_vec();
+        let x: u64 = input[0];
+        let y: u64 = input[1];
+
+        if x == y {
+            writeln!(out, "0 0").unwrap();
+            continue;
+        }
+
+        let diff: u64 = x.abs_diff(y);
+        let rem: u64 = x % diff;
+
+        writeln!(out, "{} {}", diff, rem.min(diff-rem)).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
