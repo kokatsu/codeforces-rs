d6ff15688529ae7139523b59c2a4dde1a5a964e1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-15 00:48:47 +0900

    solve problem1862b

---
+++ problemset/problem1851_1900/problem1862/src/problem1862b.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let n: usize = read();
+        let b: Vec<u64> = read_vec();
+
+        let (a, cnt): (String, u64) =
+            (1..n)
+            .fold((b[0].to_string(), 1), |(a, cnt), i| {
+                if b[i] < b[i-1] {
+                    (a+" 1 "+&b[i].to_string(), cnt+2)
+                }
+                else {
+                    (a+" "+&b[i].to_string(), cnt+1)
+                }
+            });
+
+        writeln!(out, "{}\n{}", cnt, a).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
