c79d44319d320b3727472d2bb7d89b8ab72cfdc0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-16 00:23:42 +0900

    solve problem1345b

---
+++ problemset/problem1301_1350/problem1345/src/problem1345b.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let pyramid: Vec<u64> =
+        (1..100_000)
+        .map(|i| i * (i * 3 + 1) / 2)
+        .collect();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let mut n: u64 = read();
+
+        let mut res: u64 = 0;
+        while n > 1 {
+            let p: usize = pyramid.partition_point(|&v| v <= n);
+            res += 1;
+            n -= pyramid[p-1];
+        }
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
