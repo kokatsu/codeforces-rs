5ea614253ac17ee40afcc9b28d7eb8048266d5ab
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-17 23:53:31 +0900

    solve problem1339b

---
+++ problemset/problem1301_1350/problem1339/src/problem1339b.rs
@@ -0,0 +1,49 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let n: usize = read();
+        let mut a: Vec<i64> = read_vec();
+
+        a.sort();
+
+        let mut d: Vec<i64> = vec![0; n];
+        for i in 0..(n+1)/2 {
+            d[n-i*2-1] = a[i];
+            if i != n - i - 1 {
+                d[n-i*2-2] = a[n-i-1];
+            }
+        }
+
+        let res: String =
+            (1..n)
+            .fold(d[0].to_string(), |res, i| res + " " + &d[i].to_string());
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
