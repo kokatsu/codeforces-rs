42772e4f18daf155d7586f9049af6e040879fb9b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-11-18 23:34:00 +0900

    solve problem1537c

---
+++ problemset/problem1501_1550/problem1537/src/problem1537c.rs
@@ -0,0 +1,62 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let n: usize = read();
+        let mut h: Vec<i64> = read_vec();
+
+        h.sort();
+
+        if n == 2 {
+            writeln!(out, "{} {}", h[0], h[1]).unwrap();
+            continue;
+        }
+
+        let pos: usize =
+            (1..n)
+            .fold((0, i64::MAX), |(pos, diff), i| {
+                if h[i] - h[i-1] < diff {
+                    (i, h[i]-h[i-1])
+                }
+                else {
+                    (pos, diff)
+                }
+            })
+            .0;
+
+        let s: String =
+            (pos+1..n)
+            .fold(h[pos].to_string(), |s, i| s + " " + &h[i].to_string());
+
+        let res: String =
+            (0..pos)
+            .fold(s, |res, i| res + " " + &h[i].to_string());
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
