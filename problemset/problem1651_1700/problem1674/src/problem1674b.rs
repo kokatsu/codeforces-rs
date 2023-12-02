5d59c68133287c69bfb4a1b0394e54ea42b8994c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-02 20:13:42 +0900

    solve problem1674b

---
+++ problemset/problem1651_1700/problem1674/src/problem1674b.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let s: Vec<u64> =
+            read_string()
+            .chars()
+            .into_iter()
+            .map(|c| (c as u8 - 97_u8) as u64)
+            .collect();
+
+        let res: u64 =
+            if s[0] > s[1] {
+                s[0] * 25 + s[1] + 1
+            }
+            else {
+                s[0] * 25+ s[1]
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
