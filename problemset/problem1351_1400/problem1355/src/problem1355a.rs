1d3d3fe0af63597233a9129eebc5a784830ad40a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-09 23:09:55 +0900

    solve problem1355a

---
+++ problemset/problem1351_1400/problem1355/src/problem1355a.rs
@@ -0,0 +1,52 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let input: Vec<u64> = read_vec();
+        let mut a: u64 = input[0];
+        let k: u64 = input[1];
+
+        for _ in 1..k {
+            let mut minmax: (u64, u64) = (9, 0);
+            let mut r: u64 = a;
+            while r > 0 {
+                let d: u64 = r % 10;
+                minmax = (minmax.0.min(d), minmax.1.max(d));
+                r /= 10;
+            }
+
+            if minmax.0 == 0 {
+                break;
+            }
+
+            a += minmax.0 * minmax.1;
+        }
+
+        writeln!(out, "{}", a).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
