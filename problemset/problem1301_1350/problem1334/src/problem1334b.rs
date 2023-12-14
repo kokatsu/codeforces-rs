6e7ba0d18f861fbdc1f371bb3d62a4fe5da9ccc7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-15 00:38:41 +0900

    solve problem1334b

---
+++ problemset/problem1301_1350/problem1334/src/problem1334b.rs
@@ -0,0 +1,48 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let input: Vec<usize> = read_vec();
+        let (_n, x): (usize, u64) = (input[0], input[1] as u64);
+
+        let mut a: Vec<u64> = read_vec();
+
+        a.sort();
+
+        let (mut res, mut sum): (u64, u64) = (0, 0);
+        for y in a.iter().rev() {
+            if (res + 1) * x > sum + y {
+                break;
+            }
+
+            (res, sum) = (res+1, sum+y);
+        }
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
