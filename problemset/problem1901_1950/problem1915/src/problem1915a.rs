bb0a17dab11fae59200da7703dc0a3b8da4e4022
kokatsu <66732063+kokatsu@users.noreply.github.com>
2023-12-30 11:11:03 +0900

    solve problem1915a

---
+++ problemset/problem1901_1950/problem1915/src/problem1915a.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let input: Vec<u64> = read_vec();
+        let (a, b, c): (u64, u64, u64) = (input[0], input[1], input[2]);
+
+        let res: u64 =
+            if a == b {
+                c
+            }
+            else if b == c {
+                a
+            }
+            else {
+                b
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
