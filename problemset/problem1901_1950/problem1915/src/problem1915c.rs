57f7c88e1f9fd05e660c63a694e1ad73c22e6263
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-01 21:24:18 +0900

    solve problem1915c

---
+++ problemset/problem1901_1950/problem1915/src/problem1915c.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let a: Vec<u64> = read_vec();
+
+        let s: u64 = a.iter().sum();
+        let r: u64 = (s as f64).sqrt().floor() as u64;
+
+        let res: &str =
+            if r * r == s {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
