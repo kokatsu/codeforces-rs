721ee6306ab229b5215d0d2623ffe8479d838268
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-05 19:09:26 +0900

    solve problem1914a

---
+++ problemset/problem1901_1950/problem1914/src/problem1914a.rs
@@ -0,0 +1,45 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let t: usize = read();
+
+    let mut out = BufWriter::new(stdout().lock());
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let letters: String = read_string();
+
+        let mut counts: Vec<i64> = (1..=26).collect();
+        for l in letters.chars().map(|c| c as u8 - 'A' as u8) {
+            counts[l as usize] -= 1;
+        }
+
+        let res: usize = counts
+            .into_iter()
+            .filter(|&c| c <= 0)
+            .count();
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
