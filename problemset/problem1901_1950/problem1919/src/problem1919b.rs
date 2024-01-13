2915550d145d58702b593b6d90c3d542fb3f195c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-13 22:52:26 +0900

    solve problem1919b

---
+++ problemset/problem1901_1950/problem1919/src/problem1919b.rs
@@ -0,0 +1,43 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let s: String = read_string();
+
+        let plus: usize = s
+            .chars()
+            .filter(|&c| c == '+')
+            .count();
+        let minus: usize = n - plus;
+
+        let res: usize = plus.abs_diff(minus);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
