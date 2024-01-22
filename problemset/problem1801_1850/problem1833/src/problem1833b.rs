10e57d26881f36224bf77827358e6783595888da
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-22 23:05:40 +0900

    solve problem1833b

---
+++ problemset/problem1801_1850/problem1833/src/problem1833b.rs
@@ -0,0 +1,51 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let input: Vec<usize> = read_vec();
+        let (n, _k): (usize, i64) = (input[0], input[1] as i64);
+
+        let a: Vec<i64> = read_vec();
+        let mut b: Vec<i64> = read_vec();
+
+        let mut c: Vec<(i64, usize)> = (0..n).map(|i| (a[i], i)).collect();
+
+        b.sort();
+        c.sort();
+
+        let mut d: Vec<i64> = vec![0; n];
+        for i in 0..n {
+            d[c[i].1] = b[i];
+        }
+
+        let res: String = (1..n)
+            .fold(d[0].to_string(), |res, i| res + " " + &d[i].to_string());
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
