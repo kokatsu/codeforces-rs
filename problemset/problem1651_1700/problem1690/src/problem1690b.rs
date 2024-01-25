17752adadaa3220ece7203404e32d2b664dfe0bf
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-26 00:26:08 +0900

    solve problem1690b

---
+++ problemset/problem1651_1700/problem1690/src/problem1690b.rs
@@ -0,0 +1,49 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<i64> = read_vec();
+        let b: Vec<i64> = read_vec();
+
+        let dmax: i64 = (0..n)
+            .fold(0, |dmax, i| dmax.max(a[i]-b[i]));
+
+        let res: &str = (0..n)
+            .fold("YES", |res, i| {
+                if 0.max(a[i]-dmax) == b[i] {
+                    res
+                }
+                else {
+                    "NO"
+                }
+            });
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
