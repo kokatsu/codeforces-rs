005c249488787b255a1b7872267487e12f84d9c6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-01-30 23:15:43 +0900

    solve problem0199a

---
+++ problemset/problem0151_0200/problem0199/src/problem0199a.rs
@@ -0,0 +1,51 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let n: u64 = read();
+
+    let mut fib: Vec<u64> = vec![0, 1];
+    for i in 1..100 {
+        if fib[i] > n {
+            break;
+        }
+
+        fib.push(fib[i-1]+fib[i]);
+    }
+
+    let l: usize = fib.len();
+    for i in 0..l {
+        for j in i..l {
+            for k in j..l {
+                if fib[i] + fib[j] + fib[k] == n {
+                    writeln!(out, "{} {} {}", fib[i], fib[j], fib[k]).unwrap();
+                    return;
+                }
+            }
+        }
+    }
+
+    writeln!(out, "I'm too stupid to solve this problem").unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
