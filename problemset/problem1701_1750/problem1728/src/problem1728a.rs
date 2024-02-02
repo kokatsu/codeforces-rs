05a510887591e6eb108732dbc5b054a44e6a0e36
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-02 23:34:47 +0900

    solve problem1728a

---
+++ problemset/problem1701_1750/problem1728/src/problem1728a.rs
@@ -0,0 +1,45 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let cnt: Vec<u64> = read_vec();
+
+        let max: u64 = *cnt
+            .iter()
+            .max()
+            .unwrap();
+
+        let res: usize = cnt
+            .iter()
+            .position(|&x| x == max)
+            .unwrap() + 1;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
