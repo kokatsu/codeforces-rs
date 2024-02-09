0beeb72ee5ef4e41410d4b5f6e973ae8a8303f90
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-09 23:47:26 +0900

    solve problem1927a

---
+++ problemset/problem1901_1950/problem1927/src/problem1927a.rs
@@ -0,0 +1,44 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let s: Vec<char> = read_string().chars().collect();
+
+        let b: Vec<usize> = s
+            .into_iter()
+            .enumerate()
+            .filter(|&x| x.1 == 'B')
+            .map(|x| x.0)
+            .collect();
+
+        let res: usize = b[b.len()-1] - b[0] + 1;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
