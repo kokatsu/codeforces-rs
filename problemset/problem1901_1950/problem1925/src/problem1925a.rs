fae2ef7d759ea8550f1a8ccc11701a106d48e26f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-11 23:50:34 +0900

    solve problem1925a

---
+++ problemset/problem1901_1950/problem1925/src/problem1925a.rs
@@ -0,0 +1,41 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let input: Vec<usize> = read_vec();
+        let (n, k): (usize, u8) = (input[0], input[1] as u8);
+
+        let s: String = (b'a'..b'a'+k)
+            .map(|c| c as char)
+            .collect::<String>();
+
+        let res: String = s.repeat(n);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
