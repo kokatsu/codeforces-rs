f1af82e253bbdbc03cee69437a04619a5befc595
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-13 22:51:43 +0900

    solve problem1832a

---
+++ problemset/problem1801_1850/problem1832/src/problem1832a.rs
@@ -0,0 +1,40 @@
+use std::io::{stdout, Write, BufWriter};
+use std::collections::HashSet;
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let s: Vec<char> = read_string().chars().collect();
+
+        let l: usize = s.len();
+        let set: HashSet<char> = s[0..l/2].iter().cloned().collect();
+
+        let res: &str = if set.len() > 1 { "YES" } else { "NO" };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
