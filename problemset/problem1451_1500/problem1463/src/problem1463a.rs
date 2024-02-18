57b5b1f4f9e522cfc157616e905c1a97067545cc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-02-18 23:32:46 +0900

    solve problem1463a

---
+++ problemset/problem1451_1500/problem1463/src/problem1463a.rs
@@ -0,0 +1,48 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let input: Vec<u64> = read_vec();
+        let (a, b, c): (u64, u64, u64) = (input[0], input[1], input[2]);
+
+        let sum: u64 = a + b + c;
+        let min: u64 = a.min(b).min(c);
+        let div: u64 = sum / 9;
+        let rem: u64 = sum % 9;
+
+        let res: &str =
+            if rem == 0 && min >= div {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
