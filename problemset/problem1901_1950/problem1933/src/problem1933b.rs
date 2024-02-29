f58d2b13e8f493cb5f4c6319bd91cd9543a4e27d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-01 01:07:47 +0900

    solve problem1933b

---
+++ problemset/problem1901_1950/problem1933/src/problem1933b.rs
@@ -0,0 +1,45 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let a: Vec<usize> = read_vec();
+
+        let (sum, exist_one): (usize, bool) = a
+            .iter()
+            .fold((0, false), |(sum, exist_one), x| (sum+x, exist_one|(x%3==1)));
+
+        let res: usize = match (sum%3, exist_one) {
+            (0, _) => 0,
+            (2, _) | (1, true) => 1,
+            _ => 2,
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
