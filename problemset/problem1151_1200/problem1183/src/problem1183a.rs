c71fc7904adf309c1d00fe0ef4f496512e2d46e1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-03 23:35:37 +0900

    solve problem1183a

---
+++ problemset/problem1151_1200/problem1183/src/problem1183a.rs
@@ -0,0 +1,39 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let mut a: u64 = read();
+
+    loop {
+        let d: u64 = (0..4).fold((0, a), |(d, r), _| (d+r%10, r/10)).0;
+
+        if d % 4 == 0 {
+            writeln!(out, "{}", a).unwrap();
+            return;
+        }
+
+        a += 1;
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
