aab07d446f57b376e04de2cc756a7ac4dd810df6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-04 22:26:26 +0900

    solve problem0102b

---
+++ problemset/problem0101_0150/problem0102/src/problem0102b.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let mut n: String = read_string();
+
+    let mut res: usize = 0;
+    while n.len() > 1 {
+        let t: String = n
+            .clone()
+            .chars()
+            .map(|d| d as u64 - '0' as u64)
+            .sum::<u64>()
+            .to_string();
+        n = t;
+        res += 1;
+    }
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
