9d0b7a37fcd9b7d21bdaeb61d70aebce44f12448
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-07 23:34:24 +0900

    solve problem1883b

---
+++ problemset/problem1851_1900/problem1883/src/problem1883b.rs
@@ -0,0 +1,79 @@
+use std::io::{stdout, Write, BufWriter};
+use std::collections::HashMap;
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let input: Vec<usize> = read_vec();
+        let (n, k): (usize, usize) = (input[0], input[1]);
+
+        let s: Vec<char> = read_string().chars().collect();
+
+        let map: HashMap<char, usize> = s
+            .iter()
+            .fold(HashMap::new(), |mut map, &c| {
+                *map.entry(c).or_insert(0) += 1;
+                map
+            });
+
+        let r: usize = (n - k) / 2;
+        let num: usize = map
+            .iter()
+            .map(|(_, &value)| value as usize / 2)
+            .sum::<usize>();
+
+        let res: &str =
+            if num >= r {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn run_length_encoding<T: std::cmp::PartialEq + Copy>(x: Vec<T>) -> Vec<(T, i64)> {
+    let mut ret: Vec<(T, i64)> = Vec::new();
+
+    let l: usize = x.len();
+
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+    while i < l {
+        while j < l && x[i] == x[j] {
+            j += 1usize;
+        }
+
+        ret.push((x[i], (j-i) as i64));
+        i = j;
+    }
+
+    ret
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
