b2ade22d34031ffc8437da2ac4077e1cfdd9c432
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-15 01:29:23 +0900

    solve problem1941c

---
+++ problemset/problem1901_1950/problem1941/src/problem1941c.rs
@@ -0,0 +1,45 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let s: String = read_string();
+
+        let res: usize = (0..n)
+            .fold(0, |res, i| {
+                if (i >= 2 && &s[i-2..=i] == "map") || (i + 2 < n && &s[i..=i+2] == "pie") {
+                    res + 1
+                }
+                else {
+                    res
+                }
+            });
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
