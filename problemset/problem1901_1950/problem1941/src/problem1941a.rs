5a58b338ba37ffcc209a4c3e5c5f32f5092c9a40
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-14 23:15:28 +0900

    solve problem1941a

---
+++ problemset/problem1901_1950/problem1941/src/problem1941a.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let input: Vec<usize> = read_vec();
+        let (_n, _m, k): (usize, usize, i64) = (input[0], input[1], input[2] as i64);
+
+        let b: Vec<i64> = read_vec();
+        let mut c: Vec<i64> = read_vec();
+
+        c.sort();
+
+        let res: usize =  b
+            .iter()
+            .fold(0, |res, &x| {
+                res + c.partition_point(|&u| u <= k - x)
+            });
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
