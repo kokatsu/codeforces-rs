776f3a5d2ce96f35a10689c863934e73d64e4eb2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-16 23:41:00 +0900

    solve problem1931c

---
+++ problemset/problem1901_1950/problem1931/src/problem1931c.rs
@@ -0,0 +1,71 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<i64> = read_vec();
+
+        let g: Vec<(i64, i64)> = run_length_encoding(a);
+
+        let f: (i64, i64) = *g.first().unwrap();
+        let l: (i64, i64) = *g.last().unwrap();
+
+        let res: usize =
+            if g.len() == 1 {
+                0
+            }
+            else if f.0 == l.0 {
+                n - (f.1 + l.1) as usize
+            }
+            else {
+                n - f.1.max(l.1) as usize
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn run_length_encoding<T: std::cmp::PartialEq + Copy>(x: Vec<T>) -> Vec<(T, i64)> {
+    let mut ret: Vec<(T, i64)> = Vec::new();
+
+    let l: usize = x.len();
+
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+    while i < l {
+        while j < l && x[i] == x[j] {
+            j += 1usize;
+        }
+
+        ret.push((x[i], (j-i) as i64));
+        i = j;
+    }
+
+    ret
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
