82b8eb8d52e6f183e8a2cca5c5d35163603453db
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-23 22:52:23 +0900

    solve problem1675b

---
+++ problemset/problem1651_1700/problem1675/src/problem1675b.rs
@@ -0,0 +1,48 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let mut a: Vec<u64> = read_vec();
+
+        let mut res: i64 = 0;
+        for i in (0..n-1).rev() {
+            while a[i] > 0 && a[i] >= a[i+1] {
+                a[i] /= 2;
+                res += 1;
+            }
+
+            if a[i] >= a[i+1] {
+                res = -1;
+                break;
+            }
+        }
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
