5d4c87a295de35bf563a38dc5671883abdddb561
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-29 23:45:06 +0900

    solve problem1950a

---
+++ problemset/problem1901_1950/problem1950/src/problem1950a.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let input: Vec<i64> = read_vec();
+        let (a, b, c): (i64, i64, i64) = (input[0], input[1], input[2]);
+
+        let res: &str =
+            if a < b && b < c {
+                "STAIR"
+            }
+            else if a < b && b > c {
+                "PEAK"
+            }
+            else {
+                "NONE"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
