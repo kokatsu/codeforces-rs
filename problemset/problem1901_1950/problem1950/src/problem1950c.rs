4dea6937d118cc215418db5a0bab01cda4ba81d5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-03-30 23:18:48 +0900

    solve problem1950c

---
+++ problemset/problem1901_1950/problem1950/src/problem1950c.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let s: String = read_string();
+
+        let t: Vec<&str> = s.split(":").collect();
+
+        let h: i64 = t[0].parse::<i64>().ok().unwrap();
+        let m: &str = t[1];
+
+        let res: String = match h {
+            0 => format!("12:{} AM", m),
+            12 => format!{"12:{} PM", m},
+            h if h < 12 => format!("{:02}:{} AM", h, m),
+            _ => format!("{:02}:{} PM", h-12, m),
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
