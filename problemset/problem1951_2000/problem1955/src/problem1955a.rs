5142ec71b86b0f04b959ce609f66fb3a24cd5907
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-10 08:22:15 +0900

    solve problem1955a

---
+++ problemset/problem1951_2000/problem1955/src/problem1955a.rs
@@ -0,0 +1,43 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (n, a, b): (u64, u64, u64) = {
+            let input: Vec<u64> = read_vec();
+            (input[0], input[1], input[2])
+        };
+
+        let res: u64 = match (a * 2 > b, n % 2 == 0) {
+            (true, true) => b * (n / 2),
+            (true, false) => b * (n / 2) + a,
+            _ => a * n,
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
