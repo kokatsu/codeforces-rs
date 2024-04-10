1c302b2836851c71fbd16a6bb9e443644338dc11
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-11 01:12:23 +0900

    solve problem1703d

---
+++ problemset/problem1701_1750/problem1703/src/problem1703d.rs
@@ -0,0 +1,53 @@
+use std::io::{stdout, Write, BufWriter};
+use std::collections::HashSet;
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let s: Vec<String> = (0..n).map(|_| read_string()).collect::<Vec<String>>();
+
+        let set: HashSet<String> = s.iter().cloned().collect();
+
+        let mut res: String = String::new();
+        for x in s.iter() {
+            let mut is_ok: bool = false;
+
+            let l: usize = x.len();
+            for i in 0..l {
+                if set.contains(&x[0..i]) && set.contains(&x[i..l]) {
+                    is_ok = true;
+                    break;
+                }
+            }
+
+            res += if is_ok { "1" } else { "0" };
+        }
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
