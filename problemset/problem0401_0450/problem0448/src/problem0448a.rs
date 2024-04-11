14be1a0c2cfd80b7102a24f8f4b7302f967a23f2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-12 01:42:42 +0900

    solve problem0448a

---
+++ problemset/problem0401_0450/problem0448/src/problem0448a.rs
@@ -0,0 +1,37 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let a: Vec<u64> = read_vec();
+    let b: Vec<u64> = read_vec();
+    let n: u64 = read();
+
+    let x: u64 = a.iter().sum::<u64>().div_ceil(5);
+    let y: u64 = b.iter().sum::<u64>().div_ceil(10);
+
+    let res: &str = if x + y <= n { "YES" } else { "NO" };
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
