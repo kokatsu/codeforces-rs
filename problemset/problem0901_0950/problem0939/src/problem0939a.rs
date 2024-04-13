32f73322024b4be4b2d22f9043863950aee34ebc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-13 23:58:14 +0900

    solve problem0939a

---
+++ problemset/problem0901_0950/problem0939/src/problem0939a.rs
@@ -0,0 +1,38 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let _n: usize = read();
+    let f: Vec<usize> = {
+        let g: Vec<usize> = read_vec();
+        g.iter().map(|i| i - 1).collect()
+    };
+
+    let is_ok: bool = f.iter().any(|&i| f[f[f[i]]] == i);
+
+    let res: &str = if is_ok { "YES" } else { "NO" };
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
