3f87e757cd46332e3c180c8f48546f7b3b625584
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-14 23:26:25 +0900

    solve problem1515b

---
+++ problemset/problem1501_1550/problem1515/src/problem1515b.rs
@@ -0,0 +1,51 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn is_square(x: u64) -> bool {
+    let r: u64 = (x as f64).sqrt().floor() as u64;
+    r * r == x
+}
+
+fn can_create(x: u64, y: u64) -> bool {
+    x % y == 0 && is_square(x/y)
+}
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: u64 = read();
+
+        let res: &str =
+            if can_create(n, 2) || can_create(n, 4) {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
