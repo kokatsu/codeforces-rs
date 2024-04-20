537cbc5766635f576e0de550781b0008627111c1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-21 00:04:08 +0900

    solve problem1497a

---
+++ problemset/problem1451_1500/problem1497/src/problem1497a.rs
@@ -0,0 +1,81 @@
+use std::io::{stdout, Write, BufWriter};
+use std::iter::repeat;
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let mut a: Vec<u32> = read_vec();
+
+        a.sort();
+
+        let g: Vec<(u32, i64)> = run_length_encoding(a);
+
+        let l: String = g
+            .iter()
+            .map(|x| x.0.to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        let r: String = g
+            .iter()
+            .filter(|x| x.1 > 1)
+            .map(|x| repeat(x.0.to_string()).take(x.1 as usize - 1).collect::<Vec<String>>().join(" "))
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        let res: String =
+            if r.is_empty() {
+                l
+            }
+            else {
+                l + " " + &r
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn run_length_encoding<T: std::cmp::PartialEq + Copy>(x: Vec<T>) -> Vec<(T, i64)> {
+    let mut ret: Vec<(T, i64)> = Vec::new();
+
+    let l: usize = x.len();
+
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+    while i < l {
+        while j < l && x[i] == x[j] {
+            j += 1usize;
+        }
+
+        ret.push((x[i], (j-i) as i64));
+        i = j;
+    }
+
+    ret
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
