1b9f278c404a06ab494ee1c1a46ff17704d85adc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-25 00:07:36 +0900

    solve problem1514a

---
+++ problemset/problem1501_1550/problem1514/src/problem1514a.rs
@@ -0,0 +1,44 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn is_square(x: u64) -> bool {
+    let r: u64 = (x as f64).sqrt().floor() as u64;
+    r * r == x
+}
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let a: Vec<u64> = read_vec();
+
+        let res: &str = a
+            .into_iter()
+            .fold("NO", |res, x| if is_square(x) { res } else { "YES" });
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
