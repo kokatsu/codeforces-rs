5b59fe30d43648f2af5474fd815b7e4ab21c00d2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-24 23:55:16 +0900

    solve problem1945a

---
+++ problemset/problem1901_1950/problem1945/src/problem1945a.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (a, b, c): (i64, i64, i64) = {
+            let input: Vec<i64> = read_vec();
+            (input[0], input[1], input[2])
+        };
+
+        let (d, r): (i64, i64) = (b / 3, b % 3);
+        let res: i64 =
+            if r == 0 || r + c >= 3 {
+                a + d + (c + r + 2) / 3
+            }
+            else {
+                -1
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
