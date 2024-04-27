839a517695899c1086fcb6dffce17608b0a9fb7f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-04-27 23:51:35 +0900

    solve problem1766a

---
+++ problemset/problem1751_1800/problem1766/src/problem1766a.rs
@@ -0,0 +1,41 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let mut n: u64 = read();
+
+        let mut res: u64 = 0;
+        while n >= 10 {
+            res += 9;
+            n /= 10;
+        }
+        res += n;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
