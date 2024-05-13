b6a9d23e5a69627e63de6314b44363180162ecd4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-14 02:22:47 +0900

    solve problem1907b

---
+++ problemset/problem1901_1950/problem1907/src/problem1907b.rs
@@ -0,0 +1,69 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let s: String = read_string();
+
+        let mut v: Vec<char> = s
+            .chars()
+            .rev()
+            .fold((Vec::<char>::new(), 0, 0), |(mut v, lower, upper), c| {
+                if c.is_lowercase() {
+                    if c == 'b' {
+                        (v, lower + 1, upper)
+                    }
+                    else if lower > 0 {
+                        (v, lower - 1, upper)
+                    }
+                    else {
+                        v.push(c);
+                        (v, lower, upper)
+                    }
+                }
+                else {
+                    if c == 'B' {
+                        (v, lower, upper + 1)
+                    }
+                    else if upper > 0 {
+                        (v,  lower, upper - 1)
+                    }
+                    else {
+                        v.push(c);
+                        (v, lower, upper)
+                    }
+                }
+            })
+            .0;
+
+        v.reverse();
+
+        let res: String = v.iter().collect();
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
