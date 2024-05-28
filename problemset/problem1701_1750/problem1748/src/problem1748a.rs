9d2bbe9c5837e4be5f2abad15e61729320e394f8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-05-29 02:15:53 +0900

    solve problem1748a

---
+++ problemset/problem1701_1750/problem1748/src/problem1748a.rs
@@ -0,0 +1,39 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: u64 = read();
+
+        let d: u64 = (n + 1) / 2;
+        let s: u64 = if n % 2 == 0 { d * (d + 1) } else { d * (d + 1) + d / 2 + 1 };
+
+        let res: u64 = (s as f64).sqrt().floor() as u64;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
