6959373200902842379833ee9db0cca2e659619d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-03 22:34:19 +0900

    solve problem1976a

---
+++ problemset/problem1951_2000/problem1976/src/problem1976a.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let s: String = read_string();
+
+        let res: &str = s
+            .chars()
+            .fold(("YES", '0'), |(res, pre), x| {
+                if x >= pre {
+                    (res, x)
+                }
+                else {
+                    ("NO", x)
+                }
+            })
+            .0;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
