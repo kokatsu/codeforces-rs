517c8b28cbb1f3bf1445de6bdd0031546f5edf71
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-10 03:22:48 +0900

    solve problem1699a

---
+++ problemset/problem1651_1700/problem1699/src/problem1699a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: i64 = read();
+
+        let res: String =
+            if n % 2 == 0 {
+                format!("{} 0 0", n / 2)
+            }
+            else {
+                (-1).to_string()
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
