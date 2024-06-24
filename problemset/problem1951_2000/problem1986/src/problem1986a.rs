b66837a83a857449630b39343e366a74688dc222
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-06-25 00:04:02 +0900

    solve problem1986a

---
+++ problemset/problem1951_2000/problem1986/src/problem1986a.rs
@@ -0,0 +1,37 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let x: Vec<u8> = read_vec();
+
+        let res: u8 = (1..=10)
+            .fold(u8::MAX, |res, i| x.iter().map(|u| u.abs_diff(i)).sum::<u8>().min(res));
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
