0eb93f0af5f6f2b6c83b9211f9ab07ad27186535
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-12 02:42:30 +0900

    solve problem1362a

---
+++ problemset/problem1351_1400/problem1362/src/problem1362a.rs
@@ -0,0 +1,54 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn is_power_of_two(x: u64) -> bool {
+    (x & (x - 1)) == 0
+}
+
+fn solve(x: u64, y: u64) -> i64 {
+    let d: u64 = y / x;
+    if y % x == 0 && is_power_of_two(d) {
+        let z: i64 = d.trailing_zeros() as i64;
+        return (z + 2) / 3;
+    }
+
+    -1
+}
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (a, b): (u64, u64) = {
+            let mut input: Vec<u64> = read_vec();
+            input.sort();
+            (input[0], input[1])
+        };
+
+        let res: i64 = solve(a, b);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
