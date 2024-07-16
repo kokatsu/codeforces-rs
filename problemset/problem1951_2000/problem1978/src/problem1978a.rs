dadfc01cb71367b52ccfdf557b22758b2a1dfabc
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-17 01:19:09 +0900

    solve problem1978a

---
+++ problemset/problem1951_2000/problem1978/src/problem1978a.rs
@@ -0,0 +1,39 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let mut a: Vec<u64> = read_vec();
+
+        a[0..n-1].sort();
+
+        let res: u64 = a[n-2] + a[n-1];
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
