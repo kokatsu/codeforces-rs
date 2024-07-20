c8c8ede8a0c9d36a1988a9c6538239dfa60d9d0c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-20 23:56:58 +0900

    solve problem1968c

---
+++ problemset/problem1951_2000/problem1968/src/problem1968c.rs
@@ -0,0 +1,43 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    let c: u32 = 10000;
+    for _ in 0..t {
+        let _n: usize = read();
+        let x: Vec<u32> = read_vec();
+
+        let res: String = x
+            .iter()
+            .fold((c.to_string(), c), |(res, a), u| {
+                (res + " " + &(a + u).to_string(), a + u)
+            })
+            .0;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
