b56845f9350b8404811dc5215413b45b75c2986b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-29 01:21:30 +0900

    solve problem1996b

---
+++ problemset/problem1951_2000/problem1996/src/problem1996b.rs
@@ -0,0 +1,57 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (n, k): (usize, usize) = {
+            let input: Vec<usize> = read_vec();
+            (input[0], input[1])
+        };
+
+        let res: String = (0..n)
+            .fold(String::new(), |res, i| {
+                let s: String = read_string();
+                if i % k == 0 {
+                    let s: String = s
+                        .chars()
+                        .step_by(k)
+                        .collect::<String>();
+                    if i == 0 {
+                        s
+                    }
+                    else {
+                        res + "\n" + &s
+                    }
+                }
+                else {
+                    res
+                }
+            });
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
