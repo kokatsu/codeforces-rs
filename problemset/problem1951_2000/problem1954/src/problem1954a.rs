0115ee777ba272a108275c20bdae3b9b84626233
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-07-31 01:42:53 +0900

    solve problem1954a

---
+++ problemset/problem1951_2000/problem1954/src/problem1954a.rs
@@ -0,0 +1,39 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (n, m, k): (u8, u8, u8) = {
+            let input: Vec<u8> = read_vec();
+            (input[0], input[1], input[2])
+        };
+
+        let res: &str = if (n + m - 1) / m < n - k { "YES" } else { "NO" };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
