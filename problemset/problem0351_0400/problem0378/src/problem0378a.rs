2aa85c2203eeefb26765644031a36000b64f177f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-08-15 01:46:13 +0900

    solve problem0378a

---
+++ problemset/problem0351_0400/problem0378/src/problem0378a.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, Write, BufWriter};
+use std::cmp::Ordering;
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let (a, b): (u8, u8) = {
+        let input: Vec<u8> = read_vec();
+        (input[0], input[1])
+    };
+
+    let (first, draw, second): (u8, u8, u8) = (1..=6)
+        .fold((0, 0, 0), |(first, draw, second), i| {
+            let x: u8 = a.abs_diff(i);
+            let y: u8 = b.abs_diff(i);
+            match x.cmp(&y) {
+                Ordering::Less => (first + 1, draw, second),
+                Ordering::Equal => (first, draw + 1, second),
+                _ => (first, draw, second + 1),
+            }
+        });
+
+    let res: String = format!("{} {} {}", first, draw, second);
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
