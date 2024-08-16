d90e0648df5e358ca59399930dd8f62bd38fdd83
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-08-16 23:58:28 +0900

    solve problem2000a

---
+++ problemset/problem1951_2000/problem2000/src/problem2000a.rs
@@ -0,0 +1,44 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let a: Vec<char> = read_string().chars().collect();
+
+        let l: usize = a.len();
+
+        let res: &str =
+            if l >= 3 && a[0] == '1' && a[1] == '0' && a[2] != '0' && (l != 3 || a[l-1] != '1') {
+                "YES"
+            }
+            else {
+                "NO"
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
