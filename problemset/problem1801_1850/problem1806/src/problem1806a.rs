8782817a282d254df90beba7f75abf38bddd5efe
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-08-20 00:07:20 +0900

    solve problem1806a

---
+++ problemset/problem1801_1850/problem1806/src/problem1806a.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (a, b, c, d): (i32, i32, i32, i32) = {
+            let input: Vec<i32> = read_vec();
+            (input[0], input[1], input[2], input[3])
+        };
+
+        let h: i32 = d - b;
+
+        let res: i32 =
+            if h >= 0 && a + h >= c{
+                h * 2 + a - c
+            }
+            else {
+                -1
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
