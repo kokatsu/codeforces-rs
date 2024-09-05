a3ccb1901d93fe5f087ce669a4e894f88c189d8b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-06 01:08:19 +0900

    solve problem2009b

---
+++ problemset/problem2001_2050/problem2009/src/problem2009b.rs
@@ -0,0 +1,49 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+
+        let res: String = (0..n)
+            .map(|_| {
+                read_string().chars()
+                    .enumerate()
+                    .find(|&x| x.1 == '#')
+                    .map(|x| x.0 + 1)
+                    .expect("REASON")
+                    .to_string()
+            })
+            .collect::<Vec<String>>()
+            .into_iter()
+            .rev()
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
