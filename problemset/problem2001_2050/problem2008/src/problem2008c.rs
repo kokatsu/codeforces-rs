4fbbddfa488614a8f0a56e8275f3bb774e16e214
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-09 04:02:22 +0900

    solve problem2008c

---
+++ problemset/problem2001_2050/problem2008/src/problem2008c.rs
@@ -0,0 +1,48 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let cumsum: Vec<u64> = (0..100_000u64)
+        .scan(0, |cumsum, x| {
+            *cumsum += x;
+            Some(*cumsum)
+        })
+        .collect();
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (l, r): (u64, u64) = {
+            let input: Vec<u64> = read_vec();
+            (input[0], input[1])
+        };
+
+        let d: u64 = r - l;
+
+        let res: usize = cumsum.partition_point(|x| x <= &d);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
