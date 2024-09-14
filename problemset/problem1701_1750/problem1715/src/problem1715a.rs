0dd5e48b2a33e7399dd866e0ab38f5d14d61a7cd
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-14 23:09:51 +0900

    solve problem1715a

---
+++ problemset/problem1701_1750/problem1715/src/problem1715a.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (n, m): (u32, u32) = {
+            let input: Vec<u32> = read_vec();
+            (input[0], input[1])
+        };
+
+        let (x, y): (u32, u32) = (n.min(m), n.max(m));
+
+        let res: u32 =
+            if x == 1 && y == 1 {
+                0
+            }
+            else {
+                x * 2 + y - 2
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
