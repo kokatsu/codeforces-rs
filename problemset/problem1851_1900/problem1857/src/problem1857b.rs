5c72713868d77a1bb8e724b34470fe161e56b43d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-19 03:35:17 +0900

    solve problem1857b

---
+++ problemset/problem1851_1900/problem1857/src/problem1857b.rs
@@ -0,0 +1,58 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let x: Vec<char> = read_string().chars().collect();
+
+        let l: usize = x.len();
+        let res: String =
+            if let Some(i) = x.iter().position(|x| x.to_digit(10).unwrap() >= 5) {
+                let mut j: usize = i;
+                    for &c in x[0..i].iter().rev() {
+                        if c != '4' {
+                            break;
+                        }
+                        j -= 1;
+                    }
+                let d: String =
+                    if j == 0 {
+                        "1".to_string()
+                    }
+                    else {
+                        let p: String = x[0..j-1].iter().collect();
+                        p + &(x[j-1].to_digit(10).unwrap() + 1).to_string()
+                    };
+                d + &"0".to_string().repeat(l-j)
+            }
+            else {
+                x.iter().collect()
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
