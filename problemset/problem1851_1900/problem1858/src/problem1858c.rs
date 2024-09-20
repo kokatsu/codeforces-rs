d54b115f40639b996990e47c61ef34d34c5569d0
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-20 09:25:44 +0900

    solve problem1858c

---
+++ problemset/problem1851_1900/problem1858/src/problem1858c.rs
@@ -0,0 +1,49 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+
+        let res: String = (1..=n)
+            .step_by(2)
+            .map(|i| {
+                let mut s: String = i.to_string();
+                let mut j: usize = i * 2;
+                while j <= n {
+                    s += " ";
+                    s += &j.to_string();
+                    j *= 2;
+                }
+                s
+            })
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
