7d9f410cc0f51f6fdbe746fa1b8dbe09bb924c5b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-24 02:25:09 +0900

    solve problem2013a

---
+++ problemset/problem2001_2050/problem2013/src/problem2013a.rs
@@ -0,0 +1,40 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: u64 = read();
+        let (x, y): (u64, u64) = {
+            let input: Vec<u64> = read_vec();
+            (input[0], input[1])
+        };
+
+        let res: u64 = ((n + x - 1) / x).max((n + y - 1) / y);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
