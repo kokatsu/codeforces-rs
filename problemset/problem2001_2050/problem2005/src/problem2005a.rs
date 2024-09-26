66785248454b1d9ee08d597bcce46e672aae52e4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-27 02:32:19 +0900

    solve problem2005a

---
+++ problemset/problem2001_2050/problem2005/src/problem2005a.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let vowels: &str = "aeiou";
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+
+        let res: String = vowels
+            .chars()
+            .enumerate()
+            .map(|(i, x)| {
+                let m: usize = n / 5 + if i + 1 <= n % 5 { 1 } else { 0 };
+                x.to_string().repeat(m)
+            })
+            .collect::<Vec<String>>()
+            .join("");
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
