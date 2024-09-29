52824498a3cbb960a22f42ee9ab20525754e3204
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-09-30 02:22:49 +0900

    solve problem2010b

---
+++ problemset/problem2001_2050/problem2010/src/problem2010b.rs
@@ -0,0 +1,35 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let (a, b): (u8, u8) = {
+        let input: Vec<u8> = read_vec();
+        (input[0], input[1])
+    };
+
+    let res: u8 = 6 - a - b;
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
