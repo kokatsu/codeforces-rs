4e005707cb438401fdbbfeab515c10c9a5ea271b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-10-03 01:26:50 +0900

    solve problem2020a

---
+++ problemset/problem2001_2050/problem2020/src/problem2020a.rs
@@ -0,0 +1,51 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (mut n, k): (u64, u64) = {
+            let input: Vec<u64> = read_vec();
+            (input[0], input[1])
+        };
+
+        let mut res: u64 = 0;
+        if k == 1 {
+            res = n;
+        }
+        else {
+            let mut b: u64 = k;
+            while n > 0 {
+                let m: u64 = n % b;
+                n -= m;
+                res += m / (b / k);
+                b *= k;
+            }
+        }
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
