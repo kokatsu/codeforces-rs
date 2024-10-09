e5e7884b9cd471656579aee218136a8f1fb1c3f4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-10-10 01:38:06 +0900

    solve problem1912l

---
+++ problemset/problem1901_1950/problem1912/src/problem1912l.rs
@@ -0,0 +1,52 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let n: usize = read();
+    let s: Vec<char> = read_string().chars().collect();
+
+    let mut rl: usize = s.iter().filter(|&&x| x == 'L').count();
+    let mut ro: usize = n - rl;
+
+    let (mut ll, mut lo): (usize, usize) = (0, 0);
+    let mut res: i32 = -1;
+    for &c in s[0..n-1].iter() {
+        if c == 'L' {
+            ll += 1;
+            rl -= 1;
+        }
+        else {
+            lo += 1;
+            ro -= 1;
+        }
+
+        if ll != rl && lo != ro {
+            res = (ll + lo) as i32;
+            break;
+        }
+    }
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
