28ce12ecbfae30d32f95805d0368b8b7726538c6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-10-30 01:01:40 +0900

    solve problem2030a

---
+++ problemset/problem2001_2050/problem2030/src/problem2030a.rs
@@ -0,0 +1,43 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let t: u32 = read();
+        let a: Vec<u32> = read_vec();
+
+        let (min, max): (u32, u32) = a
+            .iter()
+            .fold((u32::MAX, u32::MIN), |(min, max), &x| {
+                (min.min(x), max.max(x))
+            });
+
+        let res: u32 = (max - min) * (t - 1);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
