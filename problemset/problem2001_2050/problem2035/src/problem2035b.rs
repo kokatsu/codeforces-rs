df63bee5d9a0890e645f6f173572d03821dc157f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-11-14 01:54:49 +0900

    solve problem2035b

---
+++ problemset/problem2001_2050/problem2035/src/problem2035b.rs
@@ -0,0 +1,40 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+
+        let res: String = match n {
+            1 | 3 => "-1".to_string(),
+            x if x % 2 == 1 => "3".to_string().repeat(n - 5) + "36366",
+            _ => "3".to_string().repeat(n - 2) + "66",
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
