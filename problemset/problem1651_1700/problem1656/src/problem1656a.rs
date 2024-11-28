f30ca06dc8cfae683dc28cbeb4a20979601e5d7a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-11-28 23:00:47 +0900

    solve problem1656a

---
+++ problemset/problem1651_1700/problem1656/src/problem1656a.rs
@@ -0,0 +1,48 @@
+use std::io::{stdout, Write, BufWriter};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<u32> = read_vec();
+
+        let (mut min, mut max): (usize, usize) = (0, 0);
+        for i in 0..n {
+            if a[i] < a[min] {
+                min = i;
+            }
+
+            if a[i] > a[max] {
+                max = i;
+            }
+        }
+
+        let res: String = format!("{} {}", min + 1, max + 1);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
