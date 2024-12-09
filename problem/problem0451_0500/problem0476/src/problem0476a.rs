bfbbef80533d0ef2cfed0bf69572f753ecc74df4
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-10 03:08:17 +0900

    fix style problem0476

--- problem/problem0451_0500/problem0476/src/problem0476a.rs
+++ problem/problem0451_0500/problem0476/src/problem0476a.rs
@@ -1,22 +1,21 @@
-use std::io::{stdout, Write, BufWriter};
+use std::io::{stdout, BufWriter, Write};
 
 fn main() {
     let input: Vec<i64> = read_vec();
     let n: i64 = input[0];
     let m: i64 = input[1];
 
-    let res: i64 =
-        (0..=n/2)
+    let res: i64 = (0..=n / 2)
         .fold((-1, std::i64::MAX), |(res, num), i| {
             let rem: i64 = n - i * 2;
             let cnt: i64 = i + rem;
             if cnt % m == 0 {
                 (num.min(cnt), num.min(cnt))
-            }
-            else {
+            } else {
                 (res, num)
             }
-        }).0;
+        })
+        .0;
 
     let mut out = BufWriter::new(stdout().lock());
     writeln!(out, "{}", res).unwrap();
--- problem/problem0451_0500/problem0476/src/problem0476b.rs
+++ problem/problem0451_0500/problem0476/src/problem0476b.rs
@@ -1,49 +1,33 @@
-use std::io::{stdout, Write, BufWriter};
+use std::io::{stdout, BufWriter, Write};
 
 fn main() {
     let s1: String = read_string();
     let s2: String = read_string();
 
-    let p1: usize =
-        s1
-        .chars()
-        .filter(|&x| x == '+')
-        .count();
+    let p1: usize = s1.chars().filter(|&x| x == '+').count();
     let m1: usize = s1.len() - p1;
 
-    let p2: usize =
-        s2
-        .chars()
-        .filter(|&x| x == '+')
-        .count();
-    let m2: usize =
-        s2
-        .chars()
-        .filter(|&x| x == '-')
-        .count();
+    let p2: usize = s2.chars().filter(|&x| x == '+').count();
+    let m2: usize = s2.chars().filter(|&x| x == '-').count();
     let r2: usize = s2.len() - p2 - m2;
 
     let p: i64 = (p1 as i64) - (p2 as i64);
     let m: i64 = (m1 as i64) - (m2 as i64);
 
-    let fact: Vec<f64> =
-        (0..=s1.len())
+    let fact: Vec<f64> = (0..=s1.len())
         .scan(1.0, |num, i| {
             *num *= 1.0f64.max(i as f64);
             Some(*num)
         })
         .collect();
 
-    let res: f64 =
-        if p < 0 || m < 0 || (r2 as i64) < p + m {
-            0.0f64
-        }
-        else if p == 0 && m == 0 {
-            1.0f64
-        }
-        else {
-            fact[r2] / (fact[p as usize] * fact[m as usize] * 2.0f64.powf(r2 as f64))
-        };
+    let res: f64 = if p < 0 || m < 0 || (r2 as i64) < p + m {
+        0.0f64
+    } else if p == 0 && m == 0 {
+        1.0f64
+    } else {
+        fact[r2] / (fact[p as usize] * fact[m as usize] * 2.0f64.powf(r2 as f64))
+    };
 
     let mut out = BufWriter::new(stdout().lock());
     writeln!(out, "{:.15}", res).unwrap();
