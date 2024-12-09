1c0ab58b17d6e8042a53e49115862ab8d20bee62
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-10 03:01:10 +0900

    fix style problem0433

--- problem/problem0401_0450/problem0433/src/problem0433b.rs
+++ problem/problem0401_0450/problem0433/src/problem0433b.rs
@@ -1,12 +1,11 @@
-use std::io::{stdout, Write, BufWriter};
+use std::io::{stdout, BufWriter, Write};
 
 fn main() {
     let _n: usize = read();
     let mut v: Vec<i64> = read_vec();
 
     v.insert(0, 0);
-    let cum_v: Vec<i64> =
-        v
+    let cum_v: Vec<i64> = v
         .iter()
         .scan(0, |cumsum, x| {
             *cumsum += x;
@@ -16,8 +15,7 @@ fn main() {
 
     let mut u: Vec<i64> = v.to_vec();
     u.sort();
-    let cum_u: Vec<i64> =
-        u
+    let cum_u: Vec<i64> = u
         .iter()
         .scan(0, |cumsum, x| {
             *cumsum += x;
@@ -34,17 +32,14 @@ fn main() {
         let l: usize = input[1];
         let r: usize = input[2];
 
-        let res: i64 =
-            if t == 1 {
-                cum_v[r] - cum_v[l-1]
-            }
-            else {
-                cum_u[r] - cum_u[l-1]
-            };
+        let res: i64 = if t == 1 {
+            cum_v[r] - cum_v[l - 1]
+        } else {
+            cum_u[r] - cum_u[l - 1]
+        };
 
         writeln!(out, "{}", res).unwrap();
     }
-
 }
 
 #[allow(dead_code)]
