bdbae45db0bf06238ac7a7258b9f2c59ea97ca3b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-10 03:35:42 +0900

    fix style problem1041

--- problem/problem1001_1050/problem1041/src/problem1041a.rs
+++ problem/problem1001_1050/problem1041/src/problem1041a.rs
@@ -1,4 +1,4 @@
-use std::io::{stdout, Write, BufWriter};
+use std::io::{stdout, BufWriter, Write};
 
 fn main() {
     let mut out = BufWriter::new(stdout().lock());
@@ -8,7 +8,9 @@ fn main() {
 
     let (min, max): (usize, usize) = a
         .into_iter()
-        .fold((usize::MAX, usize::MIN), |(min, max), x| (min.min(x), max.max(x)));
+        .fold((usize::MAX, usize::MIN), |(min, max), x| {
+            (min.min(x), max.max(x))
+        });
 
     let res: usize = max - min + 1 - n;
 
