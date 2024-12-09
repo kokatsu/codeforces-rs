350f3cddf2eee1a9cb5ac00df6f96bba1b3c9b5e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-10 03:11:33 +0900

    fix style problem0545

--- problem/problem0501_0550/problem0545/src/problem0545c.rs
+++ problem/problem0501_0550/problem0545/src/problem0545c.rs
@@ -1,4 +1,4 @@
-use std::io::{stdout, Write, BufWriter};
+use std::io::{stdout, BufWriter, Write};
 
 fn main() {
     let n: usize = read();
@@ -11,20 +11,16 @@ fn main() {
         h[i] = input[1];
     }
 
-    let res: usize =
-        (0..n)
-        .fold(0, |res, i| {
-            if i == 0 || i == n -1 || x[i] - h[i] > x[i-1] {
-                res + 1
-            }
-            else if x[i] + h[i] < x[i+1] {
-                x[i] += h[i];
-                res + 1
-            }
-            else {
-                res
-            }
-        });
+    let res: usize = (0..n).fold(0, |res, i| {
+        if i == 0 || i == n - 1 || x[i] - h[i] > x[i - 1] {
+            res + 1
+        } else if x[i] + h[i] < x[i + 1] {
+            x[i] += h[i];
+            res + 1
+        } else {
+            res
+        }
+    });
 
     let mut out = BufWriter::new(stdout().lock());
     writeln!(out, "{}", res).unwrap();
