f85c9d0378aafa60e9b87be284ea693cadabab62
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-10 02:43:40 +0900

    fix style problem0219

--- problem/problem0201_0250/problem0219/src/problem0219a.rs
+++ problem/problem0201_0250/problem0219/src/problem0219a.rs
@@ -1,27 +1,24 @@
-use std::io::{stdout, Write, BufWriter};
 use std::collections::HashMap;
+use std::io::{stdout, BufWriter, Write};
 
 fn main() {
     let k: usize = read();
     let s: Vec<char> = read_string().chars().collect();
 
-    let map: HashMap<char, usize> = s
-        .iter()
-        .fold(HashMap::new(), |mut map, &c| {
-            *map.entry(c).or_insert(0) += 1;
-            map
-        });
+    let map: HashMap<char, usize> = s.iter().fold(HashMap::new(), |mut map, &c| {
+        *map.entry(c).or_insert(0) += 1;
+        map
+    });
 
-    let (t, is_ok): (String, bool) = map
-        .iter()
-        .fold((String::new(), true), |(t, is_ok), (key, val)| {
-            if is_ok && val % k == 0 {
-                (t+&key.to_string().repeat(val/k), is_ok)
-            }
-            else {
-                (t, false)
-            }
-        });
+    let (t, is_ok): (String, bool) =
+        map.iter()
+            .fold((String::new(), true), |(t, is_ok), (key, val)| {
+                if is_ok && val % k == 0 {
+                    (t + &key.to_string().repeat(val / k), is_ok)
+                } else {
+                    (t, false)
+                }
+            });
 
     let res: String = if is_ok { t.repeat(k) } else { "-1".to_string() };
 
