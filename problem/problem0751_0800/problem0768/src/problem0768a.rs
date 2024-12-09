b957cbf5867e8e2c2f893f51f24482e6e16ebb1d
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-10 03:26:31 +0900

    fix style problem0768

--- problem/problem0751_0800/problem0768/src/problem0768a.rs
+++ problem/problem0751_0800/problem0768/src/problem0768a.rs
@@ -1,32 +1,21 @@
-use std::io::{stdout, Write, BufWriter};
+use std::io::{stdout, BufWriter, Write};
 
 fn main() {
     let n: usize = read();
     let a: Vec<u64> = read_vec();
 
-    let (min, max): (u64, u64) =
-        a
+    let (min, max): (u64, u64) = a
         .iter()
         .fold((u64::MAX, 0), |(min, max), &x| (min.min(x), max.max(x)));
 
-    let min_count: usize =
-        a
-        .iter()
-        .filter(|&&x| x == min)
-        .count();
-    let max_count: usize =
-        a
-        .iter()
-        .filter(|&&x| x == max)
-        .count();
+    let min_count: usize = a.iter().filter(|&&x| x == min).count();
+    let max_count: usize = a.iter().filter(|&&x| x == max).count();
 
-    let res: usize =
-        if min == max {
-            0
-        }
-        else {
-            n - min_count - max_count
-        };
+    let res: usize = if min == max {
+        0
+    } else {
+        n - min_count - max_count
+    };
 
     let mut out = BufWriter::new(stdout().lock());
     writeln!(out, "{}", res).unwrap();
