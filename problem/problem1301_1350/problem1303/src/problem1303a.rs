ef09002fe5e033b806b8af99c5db3457b50fe2ef
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-15 11:32:29 +0900

    fix style problem1303

--- problem/problem1301_1350/problem1303/src/problem1303a.rs
+++ problem/problem1301_1350/problem1303/src/problem1303a.rs
@@ -1,4 +1,4 @@
-use std::io::{stdout, Write, BufWriter};
+use std::io::{stdout, BufWriter, Write};
 
 fn main() {
     let t: usize = read();
@@ -11,15 +11,7 @@ fn main() {
         let g: Vec<(char, i64)> = run_length_encoding(s);
 
         let res: i64 =
-            (1..g.len()-1)
-            .fold(0, |res, i| {
-                if g[i].0 == '0' {
-                    res + g[i].1
-                }
-                else {
-                    res
-                }
-            });
+            (1..g.len() - 1).fold(0, |res, i| if g[i].0 == '0' { res + g[i].1 } else { res });
 
         writeln!(out, "{}", res).unwrap();
     }
@@ -38,7 +30,7 @@ fn run_length_encoding<T: std::cmp::PartialEq + Copy>(x: Vec<T>) -> Vec<(T, i64)
             j += 1usize;
         }
 
-        ret.push((x[i], (j-i) as i64));
+        ret.push((x[i], (j - i) as i64));
         i = j;
     }
 
