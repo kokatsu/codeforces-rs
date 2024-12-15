055ea88e00edecbe24774a3c5ad930e383d59041
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-15 22:05:37 +0900

    fix style problem1453

--- problem/problem1451_1500/problem1453/src/problem1453a.rs
+++ problem/problem1451_1500/problem1453/src/problem1453a.rs
@@ -1,5 +1,5 @@
-use std::io::{stdout, Write, BufWriter};
 use std::collections::HashSet;
+use std::io::{stdout, BufWriter, Write};
 
 fn main() {
     let mut out = BufWriter::new(stdout().lock());
@@ -15,15 +15,9 @@ fn main() {
         let bottom: Vec<u8> = read_vec();
         let left: Vec<u8> = read_vec();
 
-        let set: HashSet<u8> = bottom
-            .iter()
-            .cloned()
-            .collect();
+        let set: HashSet<u8> = bottom.iter().cloned().collect();
 
-        let res: usize = left
-            .iter()
-            .filter(|x| set.contains(x))
-            .count();
+        let res: usize = left.iter().filter(|x| set.contains(x)).count();
 
         writeln!(out, "{}", res).unwrap();
     }
