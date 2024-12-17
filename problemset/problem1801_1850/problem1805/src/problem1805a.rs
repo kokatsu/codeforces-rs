0d15dbde2ba76b55caad6ed20595686e87d49920
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-18 01:26:54 +0900

    solve problem1805a

---
+++ problemset/problem1801_1850/problem1805/src/problem1805a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<i16> = read_vec();
+
+        let xor: i16 = a.iter().fold(0, |acc, x| acc ^ x);
+
+        let res: i16 = match (n % 2 == 1, xor) {
+            (true, _) => xor,
+            (false, 0) => 0,
+            _ => -1,
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
