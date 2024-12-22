6b8a815b9836b3e71822ef71f1f9b70492c135ce
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-23 02:30:18 +0900

    solve problem1249a

---
+++ problemset/problem1201_1250/problem1249/src/problem1249a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let q: usize = read();
+
+    for _ in 0..q {
+        let n: usize = read();
+        let mut a: Vec<u8> = read_vec();
+
+        a.sort();
+
+        let res: usize = if (0..n - 1).any(|i| a[i + 1] - a[i] == 1) {
+            2
+        } else {
+            1
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
