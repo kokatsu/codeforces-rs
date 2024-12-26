c4c645525e4b34df950d13462acb20866c63a3f8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2024-12-27 00:50:37 +0900

    solve problem2044b

---
+++ problemset/problem2001_2050/problem2044/src/problem2044b.rs
@@ -0,0 +1,43 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let a: String = read_string();
+
+        let res: String = a
+            .chars()
+            .rev()
+            .map(|c| match c {
+                'p' => 'q',
+                'q' => 'p',
+                _ => c,
+            })
+            .collect();
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
