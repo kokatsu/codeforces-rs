51ccfa752f875f18ceac36c7efafe75d06950a4c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-01-04 22:53:49 +0900

    solve problem0038a

---
+++ problemset/problem0001_0050/problem0038/src/problem0038a.rs
@@ -0,0 +1,36 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let _n: usize = read();
+    let d: Vec<u16> = read_vec();
+    let (a, b): (usize, usize) = {
+        let input: Vec<usize> = read_vec();
+        (input[0], input[1])
+    };
+
+    let res: u16 = d[a - 1..b - 1].iter().sum();
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
