a682b7210ed9ac004e87eebd3d0c9301e7f1baae
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-01-05 22:29:43 +0900

    solve problem1300a

---
+++ problemset/problem1251_1300/problem1300/src/problem1300a.rs
@@ -0,0 +1,43 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let a: Vec<i16> = read_vec();
+
+        let zero: i16 = a.iter().filter(|&&x| x == 0).count() as i16;
+
+        let res: i16 = zero
+            + if a.iter().sum::<i16>() + zero == 0 {
+                1
+            } else {
+                0
+            };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
