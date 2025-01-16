d1646475411e36547a075be41cedede69816691b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-01-17 00:31:04 +0900

    solve problem1605a

---
+++ problemset/problem1601_1650/problem1605/src/problem1605a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (a1, a2, a3): (u32, u32, u32) = {
+            let input: Vec<u32> = read_vec();
+            (input[0], input[1], input[2])
+        };
+
+        let res: u32 = if (a1 + a3).abs_diff(a2 * 2) % 3 == 0 {
+            0
+        } else {
+            1
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
