004aba108e91a933788ee70a876e0afe3b37cc99
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-01-18 23:57:22 +0900

    solve problem1842a

---
+++ problemset/problem1801_1850/problem1842/src/problem1842a.rs
@@ -0,0 +1,49 @@
+use std::cmp::Ordering;
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (_n, _m): (usize, usize) = {
+            let input: Vec<usize> = read_vec();
+            (input[0], input[1])
+        };
+
+        let a: Vec<u64> = read_vec();
+        let b: Vec<u64> = read_vec();
+
+        let tsondu: u64 = a.iter().sum();
+        let tenzing: u64 = b.iter().sum();
+
+        let res: &str = match tsondu.cmp(&tenzing) {
+            Ordering::Greater => "Tsondu",
+            Ordering::Less => "Tenzing",
+            Ordering::Equal => "Draw",
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
