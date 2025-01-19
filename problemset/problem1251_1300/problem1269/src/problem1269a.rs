bb1aca17ea400c677a3f156b7c9663fb936842db
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-01-20 01:58:03 +0900

    solve problem1269a

---
+++ problemset/problem1251_1300/problem1269/src/problem1269a.rs
@@ -0,0 +1,31 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let n: u64 = read();
+
+    let res: String = format!("{} {}", n * 9, n * 8);
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
