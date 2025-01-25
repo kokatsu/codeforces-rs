226e5f0a6fdbdf5ac769cd83b5ff102e9cc6e590
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-01-25 23:28:49 +0900

    solve problem1422a

---
+++ problemset/problem1401_1450/problem1422/src/problem1422a.rs
@@ -0,0 +1,38 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (a, b, c): (u32, u32, u32) = {
+            let input: Vec<u32> = read_vec();
+            (input[0], input[1], input[2])
+        };
+
+        let res: u32 = a.max(b).max(c);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
