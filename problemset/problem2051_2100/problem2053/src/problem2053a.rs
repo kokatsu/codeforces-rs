968292b409d2d8770fb55cf7fa2bbc4f2fd6d32a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-02-12 03:15:32 +0900

    solve problem2053a

---
+++ problemset/problem2051_2100/problem2053/src/problem2053a.rs
@@ -0,0 +1,40 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<u32> = read_vec();
+
+        let res: &str = if (1..n).any(|i| a[i - 1] < a[i] * 2 && a[i - 1] * 2 > a[i]) {
+            "YES"
+        } else {
+            "NO"
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
