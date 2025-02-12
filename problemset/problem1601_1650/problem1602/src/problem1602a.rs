1baea2dce2da7fcce1b243304c2e826fe5faadf1
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-02-13 01:53:41 +0900

    solve problem1602a

---
+++ problemset/problem1601_1650/problem1602/src/problem1602a.rs
@@ -0,0 +1,48 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let s: Vec<char> = read_string().chars().collect();
+
+        let l: usize = s.len();
+
+        let mut c: Vec<char> = s.clone();
+
+        c.sort();
+
+        let index: usize = (0..l).find(|&i| s[i] == c[0]).unwrap();
+
+        let res: String = format!(
+            "{} {}{}",
+            c[0],
+            s[0..index].iter().collect::<String>(),
+            s[index + 1..l].iter().collect::<String>()
+        );
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
