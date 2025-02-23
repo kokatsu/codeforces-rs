cd99eedaf33b7083a76f79569d047bf9c6293f4f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-02-23 23:53:27 +0900

    solve problem2061a

---
+++ problemset/problem2051_2100/problem2061/src/problem2061a.rs
@@ -0,0 +1,38 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: i32 = read();
+        let a: Vec<u32> = read_vec();
+
+        let odd: i32 = a.iter().filter(|&&x| x % 2 == 1).count() as i32;
+
+        let res: i32 = odd + if n == odd { -1 } else { 1 };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
