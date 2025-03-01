8a25bd33ca2467886353f04ae52526612b23ae1b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-03-01 20:47:39 +0900

    solve problem2069a

---
+++ problemset/problem2051_2100/problem2069/src/problem2069a.rs
@@ -0,0 +1,41 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let s: Vec<String> = read_vec();
+
+        let u: String = s.join("");
+
+        let res: &str = match u.find("101") {
+            Some(_) => "NO",
+            None => "YES",
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
