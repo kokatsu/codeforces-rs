36be514504f8b7f9e3e423eda9ca6f16b3e9a2b8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-03-04 02:46:20 +0900

    solve problem1552a

---
+++ problemset/problem1551_1600/problem1552/src/problem1552a.rs
@@ -0,0 +1,40 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let s: Vec<char> = read_string().chars().collect();
+
+        let mut c: Vec<char> = s.clone();
+
+        c.sort();
+
+        let res: usize = s.into_iter().zip(c).filter(|(x, y)| x != y).count();
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
