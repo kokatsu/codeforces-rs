09b9b70ef587e4f3997d3bf31f56891aa9d382e8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-03-03 15:39:42 +0900

    solve problem1684a

---
+++ problemset/problem1651_1700/problem1684/src/problem1684a.rs
@@ -0,0 +1,44 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: u32 = read();
+        let s: Vec<u32> = n
+            .to_string()
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .collect();
+
+        let res: u32 = if n < 100 {
+            s[1]
+        } else {
+            *s.iter().min().unwrap_or(&0)
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
