79bd820ac4112e30e4f9ae381e57c5057520fe79
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-03-06 00:35:32 +0900

    solve problem2072a

---
+++ problemset/problem2051_2100/problem2072/src/problem2072a.rs
@@ -0,0 +1,40 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (n, k, p): (i16, i16, i16) = {
+            let input: Vec<i16> = read_vec();
+            (input[0], input[1], input[2])
+        };
+
+        let c: i16 = (k.abs() + p - 1) / p;
+
+        let res: i16 = if c <= n { c } else { -1 };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
