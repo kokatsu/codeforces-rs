e460aa58f950b1d7eaa2953b87fa30e310322f80
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-04-11 01:03:43 +0900

    solve problem1006a

---
+++ problemset/problem1001_1050/problem1006/src/problem1006a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let _n: usize = read();
+    let a: Vec<u32> = read_vec();
+
+    let res: String = a
+        .iter()
+        .map(|&x| {
+            if x % 2 == 0 {
+                (x - 1).to_string()
+            } else {
+                x.to_string()
+            }
+        })
+        .collect::<Vec<String>>()
+        .join(" ");
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
