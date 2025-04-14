03c03c47e2155a37d8aab60d5dbb78541cbab6a5
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-04-14 23:57:38 +0900

    solve problem1556a

---
+++ problemset/problem1551_1600/problem1556/src/problem1556a.rs
@@ -0,0 +1,43 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (c, d): (i32, i32) = {
+            let input: Vec<i32> = read_vec();
+            (input[0], input[1])
+        };
+
+        let res: i32 = match (c, d) {
+            (0, 0) => 0,
+            (x, y) if x == y => 1,
+            (x, y) if x % 2 == y % 2 => 2,
+            _ => -1,
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
