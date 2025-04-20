5532bbdf88858797144525f00c95302786e3290b
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-04-20 23:25:23 +0900

    solve problem2093a

---
+++ problemset/problem2051_2100/problem2093/src/problem2093a.rs
@@ -0,0 +1,35 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let k: u16 = read();
+
+        let res: &str = if k % 2 == 1 { "YES" } else { "NO" };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
