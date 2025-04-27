b2332484e8d88a3b460eb5cdd6367a804fe268a8
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-04-28 01:01:40 +0900

    solve problem2094b

---
+++ problemset/problem2051_2100/problem2094/src/problem2094b.rs
@@ -0,0 +1,40 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (n, m, l, _r): (i16, i16, i16, i16) = {
+            let input: Vec<i16> = read_vec();
+            (input[0], input[1], input[2], input[3])
+        };
+
+        let d: i16 = l.abs().min(n - m);
+
+        let res: String = format!("{} {}", l + d, l + d + m);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
