8d08f995a47a3ba6f8549b54d11e75608193ad30
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-05-16 01:10:00 +0900

    solve problem2104a

---
+++ problemset/problem2101_2150/problem2104/src/problem2104a.rs
@@ -0,0 +1,46 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (a, b, c): (u32, u32, u32) = {
+            let input: Vec<u32> = read_vec();
+            (input[0] as u32, input[1], input[2])
+        };
+
+        let s: u32 = a + b + c;
+        let d: u32 = s / 3;
+        let r: u32 = s % 3;
+
+        let res: &str = if r == 0 && b <= d && d <= c {
+            "YES"
+        } else {
+            "NO"
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
