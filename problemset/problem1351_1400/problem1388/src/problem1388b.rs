d86b8e0290f60fe5b7406d7efa931f4e0726303f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-06-16 00:21:04 +0900

    solve problem1388b

---
+++ problemset/problem1351_1400/problem1388/src/problem1388b.rs
@@ -0,0 +1,38 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+
+        let eights: usize = n.div_ceil(4);
+        let nines: usize = n - eights;
+
+        let res: String = "9".repeat(nines).to_string() + &"8".repeat(eights);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
