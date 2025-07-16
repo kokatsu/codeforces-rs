d7d36083784e6223375ed4830d2f4cfbef676437
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-07-17 00:14:02 +0900

    solve problem1765m

---
+++ problemset/problem1751_1800/problem1765/src/problem1765m.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: u32 = read();
+
+        let s: u32 = (n as f64).sqrt().floor() as u32;
+
+        let r: u32 = (2..=s).find(|&i| n % i == 0).unwrap_or(n);
+
+        let a: u32 = n / r;
+        let b: u32 = n - a;
+
+        let res: String = format!("{} {}", a, b);
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
