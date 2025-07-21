887ec83463480957cc26abd810c71df42ccc9618
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-07-21 19:30:13 +0900

    solve problem1373c

---
+++ problemset/problem1351_1400/problem1373/src/problem1373c.rs
@@ -0,0 +1,47 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let s: Vec<char> = read_string().chars().collect();
+
+        let res: usize = s
+            .iter()
+            .enumerate()
+            .fold((s.len(), 0), |(res, num), (i, &c)| {
+                if c == '+' {
+                    (res, num + 1)
+                } else if num > 0 {
+                    (res, num - 1)
+                } else {
+                    (res + i + 1, num)
+                }
+            })
+            .0;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
