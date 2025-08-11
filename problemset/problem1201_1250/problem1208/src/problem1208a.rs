ad5db9cee6dd15db89ab76d365e447ad19063a3e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-08-12 00:23:37 +0900

    solve problem1208a

---
+++ problemset/problem1201_1250/problem1208/src/problem1208a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (a, b, n): (u32, u32, u32) = {
+            let input: Vec<u32> = read_vec();
+            (input[0], input[1], input[2])
+        };
+
+        let res: u32 = match n % 3 {
+            0 => a,
+            1 => b,
+            _ => a ^ b,
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
