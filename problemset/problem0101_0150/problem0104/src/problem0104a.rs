43b99e92b802a7b7a4e07ce143c1f721bdcb3b38
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-08-30 18:49:11 +0900

    solve problem0104a

---
+++ problemset/problem0101_0150/problem0104/src/problem0104a.rs
@@ -0,0 +1,37 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let n: u8 = read();
+
+    let res: u8 = if n == 20 {
+        15
+    } else if n > 10 && n < 22 {
+        4
+    } else {
+        0
+    };
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
