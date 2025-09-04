a5390f0cca031503d20a6b70760c562c45edaf8c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-09-05 00:42:13 +0900

    solve problem1649a

---
+++ problemset/problem1601_1650/problem1649/src/problem1649a.rs
@@ -0,0 +1,43 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let n: usize = read();
+        let a: Vec<u8> = read_vec();
+
+        let zeros: Vec<usize> = (0..n).filter(|&i| a[i] == 0).collect();
+        let l: usize = zeros.len();
+
+        let res: usize = if l == 0 {
+            0
+        } else {
+            zeros[l - 1] - zeros[0] + 2
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
