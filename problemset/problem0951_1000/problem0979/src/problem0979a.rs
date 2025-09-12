af0c2bbf7992d26cb7fec793a1170f8fcd78845f
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-09-13 01:37:48 +0900

    solve problem0979a

---
+++ problemset/problem0951_1000/problem0979/src/problem0979a.rs
@@ -0,0 +1,35 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let n: u64 = read();
+
+    let res: u64 = if n == 0 || n % 2 == 1 {
+        n.div_ceil(2)
+    } else {
+        n + 1
+    };
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
