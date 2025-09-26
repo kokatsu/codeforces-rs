befe37d8232823bbcc3a3b81b16a37c71dff8df9
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-09-26 23:39:08 +0900

    solve problem1175a

---
+++ problemset/problem1151_1200/problem1175/src/problem1175a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (mut n, k): (u64, u64) = {
+            let input: Vec<u64> = read_vec();
+            (input[0], input[1])
+        };
+
+        let mut res: u64 = 0;
+        while n > 0 {
+            res += n % k + if n < k { 0 } else { 1 };
+            n /= k;
+        }
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
