d9a6d457a66051fd04fb9206b2f55addbc3d120c
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-10-02 00:52:47 +0900

    solve problem0869b

---
+++ problemset/problem0851_0900/problem0869/src/problem0869b.rs
@@ -0,0 +1,41 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let (a, b): (u64, u64) = {
+        let input: Vec<u64> = read_vec();
+        (input[0], input[1])
+    };
+
+    let d: u64 = b - a;
+
+    let res: u64 = if d >= 5 {
+        0
+    } else {
+        let m: u64 = a % 10;
+        (1..=d).fold(1, |acc, x| (acc * ((m + x) % 10)) % 10)
+    };
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
