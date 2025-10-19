60bacb770a404a3511e0b978bff5d43d315a0e20
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-10-20 01:34:04 +0900

    solve problem2149a

---
+++ problemset/problem2101_2150/problem2149/src/problem2149a.rs
@@ -0,0 +1,45 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let a: Vec<i8> = read_vec();
+
+        let (z, m): (usize, usize) = a.iter().fold((0, 0), |(mut z, mut m), &x| {
+            if x == 0 {
+                z += 1;
+            } else if x == -1 {
+                m += 1;
+            }
+            (z, m)
+        });
+
+        let res: usize = z + m % 2 * 2;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
