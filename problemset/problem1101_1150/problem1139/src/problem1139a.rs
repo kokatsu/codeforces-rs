c5ba6c91a8d9c983d21d59f93807d36d163c51fa
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-10-28 00:18:04 +0900

    solve problem1139a

---
+++ problemset/problem1101_1150/problem1139/src/problem1139a.rs
@@ -0,0 +1,36 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let _n: usize = read();
+    let s: Vec<u8> = read_string().chars().map(|c| c as u8 - b'0').collect();
+
+    let res: u64 = s
+        .iter()
+        .enumerate()
+        .map(|(i, &c)| if c % 2 == 0 { (i + 1) as u64 } else { 0 })
+        .sum();
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
