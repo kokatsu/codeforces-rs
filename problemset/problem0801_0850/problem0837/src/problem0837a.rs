53d354741009541bf6db5488c9790ac9f72bf070
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-11-18 00:23:22 +0900

    solve problem0837a

---
+++ problemset/problem0801_0850/problem0837/src/problem0837a.rs
@@ -0,0 +1,39 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let _n: usize = read();
+    let s: Vec<String> = read_string()
+        .split_whitespace()
+        .map(|v| v.to_string())
+        .collect();
+
+    let res: usize = s
+        .iter()
+        .map(|v| v.chars().filter(|&x| x.is_uppercase()).count())
+        .max()
+        .unwrap();
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
