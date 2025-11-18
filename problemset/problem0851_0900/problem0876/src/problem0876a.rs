608a9cbe4e3f5d21dbe51ecc5adc96f586344298
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-11-19 01:11:37 +0900

    solve problem0876a

---
+++ problemset/problem0851_0900/problem0876/src/problem0876a.rs
@@ -0,0 +1,36 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let n: u16 = read();
+    let a: u16 = read();
+    let b: u16 = read();
+    let c: u16 = read();
+
+    let res: u16 = (a * (n - 1))
+        .min(b * (n - 1))
+        .min(a.min(b) + c * (n.saturating_sub(2)));
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
