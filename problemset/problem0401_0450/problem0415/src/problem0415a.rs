b7beb28abe473d64db5ea2357df9085035274cfb
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-11-24 21:58:07 +0900

    solve problem0415a

---
+++ problemset/problem0401_0450/problem0415/src/problem0415a.rs
@@ -0,0 +1,49 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let (n, _m): (usize, usize) = {
+        let input: Vec<usize> = read_vec();
+        (input[0], input[1])
+    };
+
+    let b: Vec<usize> = read_vec();
+
+    let mut a: Vec<usize> = vec![0; n];
+    for &x in b.iter() {
+        for y in a.iter_mut().take(n).skip(x - 1) {
+            if *y == 0 {
+                *y = x;
+            }
+        }
+    }
+
+    let res: String = a
+        .iter()
+        .map(|&x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
