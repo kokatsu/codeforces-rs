71d6e7f5e5bd58b4b920812d8eb6e583478bdec6
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-11-28 01:57:48 +0900

    solve problem0306a

---
+++ problemset/problem0301_0350/problem0306/src/problem0306a.rs
@@ -0,0 +1,37 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let (n, m): (u8, u8) = {
+        let input: Vec<u8> = read_vec();
+        (input[0], input[1])
+    };
+
+    let res: String = (0..m)
+        .map(|i| if i < n % m { n / m + 1 } else { n / m }.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
