c3376f6f78fe2bd24757c3f5b065914e04af0367
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-11-29 23:41:48 +0900

    solve problem0045a

---
+++ problemset/problem0001_0050/problem0045/src/problem0045a.rs
@@ -0,0 +1,49 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let names: [&str; 12] = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    let s: String = read_string();
+    let k: usize = read();
+
+    let index: usize = names.iter().position(|&name| name == s).unwrap();
+
+    let res: &str = names[(index + k) % 12];
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
