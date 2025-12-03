66da44550e12536e2d56ebf1ead374192087f5e9
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-12-04 00:52:35 +0900

    solve problem2167b

---
+++ problemset/problem2151_2200/problem2167/src/problem2167b.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let q: usize = read();
+
+    for _ in 0..q {
+        let _n: usize = read();
+        let (mut s, mut t): (Vec<char>, Vec<char>) = {
+            let input = read_vec::<String>();
+            (input[0].chars().collect(), input[1].chars().collect())
+        };
+
+        s.sort();
+        t.sort();
+
+        let res: &str = if s == t { "YES" } else { "NO" };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
