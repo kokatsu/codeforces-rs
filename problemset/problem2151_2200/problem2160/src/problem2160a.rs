fe4a0652a5668693e7cdd318d500b2e261b5db61
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-12-14 00:11:01 +0900

    solve problem2160a

---
+++ problemset/problem2151_2200/problem2160/src/problem2160a.rs
@@ -0,0 +1,45 @@
+use std::collections::HashSet;
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let a: Vec<u8> = read_vec();
+
+        let s: HashSet<u8> = a.iter().cloned().collect();
+
+        let mut res: u8 = 0;
+        for i in 0..=100 {
+            if !s.contains(&i) {
+                res = i;
+                break;
+            }
+        }
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
