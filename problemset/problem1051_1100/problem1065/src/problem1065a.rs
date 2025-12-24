9ef0c0a2d95918e22c95a2230c6affcb5a5fa7d7
kokatsu <66732063+kokatsu@users.noreply.github.com>
2025-12-25 01:13:49 +0900

    solve problem1065a

---
+++ problemset/problem1051_1100/problem1065/src/problem1065a.rs
@@ -0,0 +1,40 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (s, a, b, c): (u64, u64, u64, u64) = {
+            let input = read_vec::<u64>();
+            (input[0], input[1], input[2], input[3])
+        };
+
+        let d: u64 = s / c;
+
+        let res: u64 = d + d / a * b;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
