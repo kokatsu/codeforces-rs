01be50152732d4d6840303f0279035ffb2ae981e
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-01-12 22:18:45 +0900

    solve problem2182b

---
+++ problemset/problem2151_2200/problem2182/src/problem2182b.rs
@@ -0,0 +1,59 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn get_cake_layers(x: u32, y: u32) -> u32 {
+    let (mut u, mut v) = (x, y);
+    let mut size = 1;
+    let mut ret = 0;
+    for i in 0..32 {
+        if i % 2 == 0 {
+            if u < size {
+                break;
+            }
+            u -= size;
+        } else {
+            if v < size {
+                break;
+            }
+            v -= size;
+        }
+        size *= 2;
+        ret += 1;
+    }
+    ret
+}
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (a, b): (u32, u32) = {
+            let input: Vec<u32> = read_vec();
+            (input[0], input[1])
+        };
+
+        let res = get_cake_layers(a, b).max(get_cake_layers(b, a));
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
