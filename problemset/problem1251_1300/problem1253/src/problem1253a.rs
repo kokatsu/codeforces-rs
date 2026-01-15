3b94b087fc702a2da71e8c380e6a35b9fd1bdf86
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-01-16 01:02:16 +0900

    solve problem1253a

---
+++ problemset/problem1251_1300/problem1253/src/problem1253a.rs
@@ -0,0 +1,68 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let _n: usize = read();
+        let a: Vec<i16> = read_vec();
+        let b: Vec<i16> = read_vec();
+
+        let d: Vec<i16> = a.iter().zip(b.iter()).map(|(x, y)| y - x).collect();
+
+        let rle: Vec<(i16, usize)> = run_length_encoding(d);
+
+        let res: &str = if rle.len() <= 3
+            && rle.iter().all(|(x, _)| *x >= 0i16)
+            && rle.iter().filter(|(x, _)| *x > 0i16).count() <= 1usize
+        {
+            "YES"
+        } else {
+            "NO"
+        };
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn run_length_encoding<T: std::cmp::PartialEq + Copy>(x: Vec<T>) -> Vec<(T, usize)> {
+    let mut ret: Vec<(T, usize)> = Vec::new();
+
+    let l: usize = x.len();
+
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+    while i < l {
+        while j < l && x[i] == x[j] {
+            j += 1usize;
+        }
+
+        ret.push((x[i], j - i));
+        i = j;
+    }
+
+    ret
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
