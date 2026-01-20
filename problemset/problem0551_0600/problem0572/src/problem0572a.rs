61dacc68eef31348de5c61d94ab30cefbb45ef04
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-01-21 00:42:43 +0900

    solve problem0572a

---
+++ problemset/problem0551_0600/problem0572/src/problem0572a.rs
@@ -0,0 +1,42 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let (_na, nb): (usize, usize) = {
+        let input: Vec<usize> = read_vec();
+        (input[0], input[1])
+    };
+
+    let (k, m): (usize, usize) = {
+        let input: Vec<usize> = read_vec();
+        (input[0], input[1])
+    };
+
+    let a: Vec<i32> = read_vec();
+    let b: Vec<i32> = read_vec();
+
+    let res = if a[k - 1] < b[nb - m] { "YES" } else { "NO" };
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
