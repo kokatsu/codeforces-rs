9629d466e18a71707d0482cff4e1734aed8056a3
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-01-25 15:09:25 +0900

    solve problem1236a

---
+++ problemset/problem1201_1250/problem1236/src/problem1236a.rs
@@ -0,0 +1,40 @@
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let t: usize = read();
+
+    for _ in 0..t {
+        let (a, b, c): (u16, u16, u16) = {
+            let input: Vec<u16> = read_vec();
+            (input[0], input[1], input[2])
+        };
+
+        let d: u16 = b.min(c / 2);
+
+        let res: u16 = (d + a.min((b - d) / 2)) * 3;
+
+        writeln!(out, "{}", res).unwrap();
+    }
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
