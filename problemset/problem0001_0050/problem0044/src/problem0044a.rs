045209181cffb1cf09941645a109f17b443926b2
kokatsu <66732063+kokatsu@users.noreply.github.com>
2026-01-29 00:24:25 +0900

    solve problem0044a

---
+++ problemset/problem0001_0050/problem0044/src/problem0044a.rs
@@ -0,0 +1,39 @@
+use std::collections::HashSet;
+use std::io::{stdout, BufWriter, Write};
+
+fn main() {
+    let mut out = BufWriter::new(stdout().lock());
+
+    let n: usize = read();
+
+    let mut set: HashSet<String> = HashSet::new();
+
+    for _ in 0..n {
+        let s: String = read_string();
+        set.insert(s);
+    }
+
+    let res: usize = set.len();
+
+    writeln!(out, "{}", res).unwrap();
+}
+
+#[allow(dead_code)]
+fn read_string() -> String {
+    let mut s: String = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().to_string()
+}
+
+#[allow(dead_code)]
+fn read<T: std::str::FromStr>() -> T {
+    read_string().parse().ok().unwrap()
+}
+
+#[allow(dead_code)]
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_string()
+        .split_whitespace()
+        .map(|v| v.parse().ok().unwrap())
+        .collect()
+}
